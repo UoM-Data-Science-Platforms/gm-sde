b6feed974372d23573574a6c3a7f5fcf41f719c7
Richard Williams <richard.williams2@manchester.ac.uk>
2021-03-25 16:27:12 +0000

    Migrated RQ025 project to new data extraction process

--- projects/025 - Watkinson/extract-data.bat
+++ projects/025 - Watkinson/extract-data.bat
@@ -4,15 +4,26 @@ REM ┌────────────────────────�
 REM │ Windows batch script to extract the data files │
 REM └────────────────────────────────────────────────┘
 
-@ECHO OFF
-
 REM move to batch dir 
 cd /d %~dp0
 
-REM for each sql file execute against the HDM_Research database using MFA Azure AD
-forfiles /p extraction-sql /s /m *.sql /c "cmd /c sqlcmd -G -d HDM_Research -i @path -W -s , -h -1 -o @fname"
+REM Call npm install to ensure up to date
+cd scripts
+call npm i --quiet
 
-REM each output file move to data directory and add a .txt extension
-forfiles /p extraction-sql /s /m * /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../output-data-files/@fname.txt"
+REM Return to project root
+cd ..
+
+REM Main script called via npm start
+call node scripts/main.js
 
 pause
+
+REM OLD WAY KEEP FOR NOW
+REM set USERNAME=richard.williams@grhapp.com
+REM set /p USERNAME=Enter your username (e.g. richard.williams@grhapp.com):
+REM REM for each sql file execute against the HDM_Research database using MFA Azure AD
+REM forfiles /p extraction-sql /s /m *.sql /c "cmd /c sqlcmd -S GM-ccbi-live-01.database.windows.net -G -U %USERNAME% -l 30 -d HDM_Research -i @path -W -s , -h -1 -o @fname"
+REM REM each output file move to data directory and add a .txt extension
+REM forfiles /p extraction-sql /s /m * /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../output-data-files/@fname.txt"
+REM pause
