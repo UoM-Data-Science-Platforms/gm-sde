c9b764469c6a864619544c6915bca5457a8d6c0d
Richard Williams <richard.williams2@manchester.ac.uk>
2021-03-30 14:49:01 +0100

    Linux/mac version of run all script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# ┌───────────────────────────────────────────────┐
+# │ Linux/mac bash script to call the main script │
+# └───────────────────────────────────────────────┘
+
+npm i --quiet --production
+
+npm start
+
+read -p "Press [Enter] to continue..."
