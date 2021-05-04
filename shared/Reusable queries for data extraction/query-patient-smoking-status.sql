8be71a5e7a4f149b74e743b882e295d74e52e9e5
Richard Williams <richard.williams2@manchester.ac.uk>
2021-05-04 14:04:12 +0100

    Added query to get smoking status

---
+++ shared/Reusable queries for data extraction/query-patient-smoking-status.sql
@@ -0,0 +1,141 @@
+--┌────────────────┐
+--│ Smoking status │
+--└────────────────┘
+
+-- OBJECTIVE: To get the smoking status for each patient in a cohort.
+
+-- INPUT: Assumes there exists a temp table as follows:
+-- #Patients (FK_Patient_Link_ID)
+--  A distinct list of FK_Patient_Link_IDs for each patient in the cohort
+
+-- OUTPUT: A temp table as follows:
+-- #PatientSmokingStatus (FK_Patient_Link_ID, PassiveSmoker, WorstSmokingStatus, CurrentSmokingStatus)
+-- 	- FK_Patient_Link_ID - unique patient id
+--	-	PassiveSmoker - Y/N (whether a patient has ever had a code for passive smoking)
+--	-	WorstSmokingStatus - [non-trivial-smoker/trivial-smoker/non-smoker]
+--	-	CurrentSmokingStatus - [non-trivial-smoker/trivial-smoker/non-smoker]
+
+-- ASSUMPTIONS:
+--	- We take the most recent smoking status in a patient's record to be correct
+--	-	However, there is likely confusion between the "non smoker" and "never smoked" codes. Especially as sometimes the synonyms for these codes overlap. Therefore, a patient wih a most recent smoking status of "never", but who has previous smoking codes, would be classed as WorstSmokingStatus=non-trivial-smoker / CurrentSmokingStatus=non-smoker
+
+--> CODESETS smoking-status-current smoking-status-currently-not smoking-status-ex smoking-status-ex-trivial smoking-status-never smoking-status-passive smoking-status-trivial
+-- Get all patients year of birth for the cohort
+IF OBJECT_ID('tempdb..#AllPatientSmokingStatusCodes') IS NOT NULL DROP TABLE #AllPatientSmokingStatusCodes;
+SELECT 
+	FK_Patient_Link_ID,
+	CAST(EventDate AS DATE) AS EventDate,
+	FK_Reference_Coding_ID,
+	FK_Reference_SnomedCT_ID
+INTO #AllPatientSmokingStatusCodes
+FROM RLS.vw_GP_Events
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+AND (
+	FK_Reference_SnomedCT_ID IN (
+		SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets 
+		WHERE Concept IN (
+			'smoking-status-current',
+			'smoking-status-currently-not',
+			'smoking-status-ex',
+			'smoking-status-ex-trivial',
+			'smoking-status-never',
+			'smoking-status-passive',
+			'smoking-status-trivial'
+		)
+		AND [Version]=1
+	) OR
+  FK_Reference_Coding_ID IN (
+		SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets 
+		WHERE Concept IN (
+			'smoking-status-current',
+			'smoking-status-currently-not',
+			'smoking-status-ex',
+			'smoking-status-ex-trivial',
+			'smoking-status-never',
+			'smoking-status-passive',
+			'smoking-status-trivial'
+		)
+		AND [Version]=1
+	)
+);
+
+IF OBJECT_ID('tempdb..#AllPatientSmokingStatusConcept') IS NOT NULL DROP TABLE #AllPatientSmokingStatusConcept;
+SELECT 
+	a.FK_Patient_Link_ID,
+	EventDate,
+	CASE WHEN c.Concept IS NULL THEN s.Concept ELSE c.Concept END AS Concept,
+	-1 AS Severity
+INTO #AllPatientSmokingStatusConcept
+FROM #AllPatientSmokingStatusCodes a
+LEFT OUTER JOIN #VersionedCodeSets c on c.FK_Reference_Coding_ID = a.FK_Reference_Coding_ID
+LEFT OUTER JOIN #VersionedSnomedSets s on s.FK_Reference_SnomedCT_ID = a.FK_Reference_SnomedCT_ID;
+
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 2
+WHERE Concept IN ('smoking-status-current');
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 2
+WHERE Concept IN ('smoking-status-ex');
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 1
+WHERE Concept IN ('smoking-status-ex-trivial');
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 1
+WHERE Concept IN ('smoking-status-trivial');
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 0
+WHERE Concept IN ('smoking-status-never');
+UPDATE #AllPatientSmokingStatusConcept
+SET Severity = 0
+WHERE Concept IN ('smoking-status-currently-not');
+
+select * from #AllPatientSmokingStatusConcept where Severity < 0;
+
+-- passive smokers
+IF OBJECT_ID('tempdb..#TempPassiveSmokers') IS NOT NULL DROP TABLE #TempPassiveSmokers;
+select DISTINCT FK_Patient_Link_ID into #TempPassiveSmokers from #AllPatientSmokingStatusConcept
+where Concept = 'smoking-status-passive';
+
+-- For "worst" smoking status
+IF OBJECT_ID('tempdb..#TempWorst') IS NOT NULL DROP TABLE #TempWorst;
+SELECT 
+	FK_Patient_Link_ID, 
+	CASE 
+		WHEN MAX(Severity) = 2 THEN 'non-trivial-smoker'
+		WHEN MAX(Severity) = 1 THEN 'trivial-smoker'
+		WHEN MAX(Severity) = 0 THEN 'non-smoker'
+	END AS [Status]
+INTO #TempWorst
+FROM #AllPatientSmokingStatusConcept
+WHERE Severity >= 0
+GROUP BY FK_Patient_Link_ID;
+
+-- For "current" smoking status
+IF OBJECT_ID('tempdb..#TempCurrent') IS NOT NULL DROP TABLE #TempCurrent;
+SELECT 
+	a.FK_Patient_Link_ID, 
+	CASE 
+		WHEN MAX(Severity) = 2 THEN 'non-trivial-smoker'
+		WHEN MAX(Severity) = 1 THEN 'trivial-smoker'
+		WHEN MAX(Severity) = 0 THEN 'non-smoker'
+	END AS [Status]
+INTO #TempCurrent
+FROM #AllPatientSmokingStatusConcept a
+INNER JOIN (
+	SELECT FK_Patient_Link_ID, MAX(EventDate) AS MostRecentDate FROM #AllPatientSmokingStatusConcept
+	WHERE Severity >= 0
+	GROUP BY FK_Patient_Link_ID
+) sub ON sub.MostRecentDate = a.EventDate and sub.FK_Patient_Link_ID = a.FK_Patient_Link_ID
+GROUP BY a.FK_Patient_Link_ID;
+
+-- Bring all together in final table
+IF OBJECT_ID('tempdb..#PatientSmokingStatus') IS NOT NULL DROP TABLE #PatientSmokingStatus;
+SELECT 
+	p.FK_Patient_Link_ID,
+	CASE WHEN ps.FK_Patient_Link_ID IS NULL THEN 'N' ELSE 'Y' END AS PassiveSmoker,
+	CASE WHEN w.[Status] IS NULL THEN 'non-smoker' ELSE w.[Status] END AS WorstSmokingStatus,
+	CASE WHEN c.[Status] IS NULL THEN 'non-smoker' ELSE c.[Status] END AS CurrentSmokingStatus
+INTO #PatientSmokingStatus FROM #Patients p
+LEFT OUTER JOIN #TempPassiveSmokers ps on ps.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+LEFT OUTER JOIN #TempWorst w on w.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+LEFT OUTER JOIN #TempCurrent c on c.FK_Patient_Link_ID = p.FK_Patient_Link_ID;
