4401d8d06bb81cca2b5b999eecf3427c20aa436f
Richard Williams <richard.williams2@manchester.ac.uk>
2021-05-11 09:52:04 +0100

    Final Heald changes

--- projects/020 - Heald/template-sql/hba1c.template.sql
+++ projects/020 - Heald/template-sql/hba1c.template.sql
@@ -5,6 +5,14 @@
 -- For each patient with a COVID positive test, this produces 2 years of hbA1c readings
 -- leading up to the date of the positive test.
 
+-- Set the start date
+DECLARE @StartDate datetime;
+SET @StartDate = '2020-01-01';
+
+-- Only need at most hba1c from 2 years prior to COVID test
+DECLARE @EventsFromDate datetime;
+SET @EventsFromDate = DATEADD(year, -2, @StartDate);
+
 --> CODESET hba1c
 
 -- Get all covid positive patients as this is the population of the matched cohort
@@ -12,7 +20,7 @@ IF OBJECT_ID('tempdb..#CovidPatients') IS NOT NULL DROP TABLE #CovidPatients;
 SELECT FK_Patient_Link_ID, MIN(CONVERT(DATE, [EventDate])) AS FirstCovidPositiveDate INTO #CovidPatients
 FROM [RLS].[vw_COVID19]
 WHERE GroupDescription = 'Confirmed'
-AND EventDate > '2020-01-01'
+AND EventDate > @StartDate
 AND EventDate <= GETDATE()
 GROUP BY FK_Patient_Link_ID;
 
@@ -29,7 +37,7 @@ WHERE (
   FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE (Concept IN ('hba1c') AND [Version]=2))
 )
 AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #CovidPatients)
-AND EventDate > '2018-01-01'
+AND EventDate > @EventsFromDate
 AND [Value] IS NOT NULL
 AND [Value] != '0';
 
