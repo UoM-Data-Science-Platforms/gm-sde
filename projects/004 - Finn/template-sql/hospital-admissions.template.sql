3a354e4185a02005f5e465b8eb20abfdc01ef372
Iliada Eleftheriou <iliada.eleftheriou@manchester.ac.uk>
2021-07-10 19:08:05 +0100

    Finalise medications and hospital admissions extract files

--- projects/004 - Finn/template-sql/hospital-admissions.template.sql
+++ projects/004 - Finn/template-sql/hospital-admissions.template.sql
@@ -6,16 +6,12 @@
 
 -- Hospital admissions for the all the cohort patients who had covid
 
-
 -- OUTPUT: A single table with the following:
---	FK: PatientId
---	AdmissionDate 
---	Admission Type Code
---	AdmissionTypeDescription
---	ReasonForAdmissionCode 
---	ReasonForAdmissionDescription 
---	LengthOfStay 
---	Discharge date
+--	PatientId (Int)
+--	AdmissionDate (YYYY-MM-DD)
+--	Admission Type (Unplanned, Planned, Maternity, Transfer, Other)
+--	LengthOfStay (Int)
+--	DischargeDate (YYYY-MM-DD)
 
 
 --Just want the output, not the messages
@@ -27,14 +23,7 @@ SET @StartDate = '2020-02-01';
 
 -- Get all the patients in the cohort
 --> EXECUTE query-cancer-cohort-matching.sql
--- OUTPUTS: #Patients2
-
-
-IF OBJECT_ID('tempdb..#Patients') IS NOT NULL DROP TABLE #Patients;
-SELECT DISTINCT FK_Patient_Link_ID
-INTO #Patients
-FROM #Patients2;
-
+-- OUTPUTS: #Patients, #Patients2
 
 --> EXECUTE query-classify-secondary-admissions.sql
 -- OUTPUT: #AdmissionTypes (FK_Patient_Link_ID, AdmissionDate, AcuteProvider, AdmissionType)
@@ -45,23 +34,19 @@ FROM #Patients2;
 -- #Admissions (FK_Patient_Link_ID, AdmissionDate, AcuteProvider)
 
 --> EXECUTE query-admissions-covid-utilisation.sql
--- #COVIDUtilisationAdmissions (FK_Patient_Link_ID, AdmissionDate, AcuteProvider, CovidHealthcareUtilisation)
+-- OUTPUT: #COVIDUtilisationAdmissions (FK_Patient_Link_ID, AdmissionDate, AcuteProvider, CovidHealthcareUtilisation)
 
-IF OBJECT_ID('tempdb..#HospitalAdmissions') IS NOT NULL DROP TABLE #HospitalAdmissions;
 SELECT 
-    a.FK_Patient_Link_ID AS PatientId,
-    a.AdmissionDate,
-    AdmissionType,
+    c.FK_Patient_Link_ID AS PatientId,
+    c.AdmissionDate,
+    a.AdmissionType,
     l.DischargeDate,
     l.LengthOfStay
-FROM #AdmissionTypes p
-    LEFT OUTER JOIN #Admissions a ON a.FK_Patient_Link_ID = p.FK_Patient_Link_ID AND a.AdmissionDate = p.AdmissionDate AND a.AcuteProvider = p.AcuteProvider
-    LEFT OUTER JOIN #COVIDUtilisationAdmissions c ON c.FK_Patient_Link_ID = p.FK_Patient_Link_ID AND c.AdmissionDate = p.AdmissionDate AND c.AcuteProvider = p.AcuteProvider
-    LEFT OUTER JOIN #LengthOfStay l ON l.FK_Patient_Link_ID = p.FK_Patient_Link_ID AND l.AdmissionDate = p.AdmissionDate AND l.AcuteProvider = p.AcuteProvider
+FROM #COVIDUtilisationAdmissions c
+LEFT OUTER JOIN #AdmissionTypes a ON a.FK_Patient_Link_ID = c.FK_Patient_Link_ID AND a.AdmissionDate = c.AdmissionDate 
+LEFT OUTER JOIN #LengthOfStay l ON l.FK_Patient_Link_ID = c.FK_Patient_Link_ID AND l.AdmissionDate = c.AdmissionDate 
 WHERE c.CovidHealthcareUtilisation = 'TRUE';
--- GROUP BY p.AdmissionDate,p.AcuteProvider
 
--- 2.354 rows
 
 
 
--- projects/004 - Finn/template-sql/medications.template.sql
+++ projects/004 - Finn/template-sql/medications.template.sql
@@ -5,17 +5,16 @@
 -- All medications for all patients in the study cohort one year before the index date.
 
 -- OUTPUT: Data with the following fields
---     PK: MedicationID
---     FK: PatientID
---     FirstMedicationDate
---     LastMedicationDate 
---     Reference Coding ID 
---     Supplied Code 
---     Quantity
---     Dosage 
---     Last Issue Date
+--     PatientId (Int)
+--     MedicationDate (YYYY-MM-DD)
+--     FirstMedicationDate (YYYY-MM-DD)
+--     LastMedicationDate (YYYY-MM-DD)
+--     DrugCode (Nvarchar)
+--     Quantity (Nvarchar)
+--     Dosage (Nvarchar)
+--     Last Issue Date (YYYY-MM-DD)
 --     RepeatMedicationFlag e.g. (Y, N, Null)
---     SnomedCT Identifier
+
 
 
 --Just want the output, not the messages
@@ -23,31 +22,25 @@ SET NOCOUNT ON;
 
 -- Set the start date
 DECLARE @StartDate datetime;
-SET @StartDate = '2019-02-01';
+SET @StartDate = '2020-02-01';
 
 -- Get all the patients in the cohort
 --> EXECUTE query-cancer-cohort-matching.sql
--- OUTPUTS: #Patients2
+-- OUTPUTS: #Patients, #Patients2
 
 
 SELECT 
 	FK_Patient_Link_ID AS PatientId,
 	CAST(MedicationDate AS DATE) AS MedicationDate,
-	SuppliedCode,
-	MedicationDescription,
-	CASE WHEN s.Concept IS NULL THEN c.Concept ELSE s.Concept END AS DrugType,
+	SuppliedCode AS DrugCode,
 	Quantity,
 	Dosage,
 	LastIssueDate,
-	Units, --??? 
-	RepeatMedicationFlag
-FROM RLS.vw_GP_Medications m
-LEFT OUTER JOIN #VersionedSnomedSets s ON s.FK_Reference_SnomedCT_ID = m.FK_Reference_SnomedCT_ID
-LEFT OUTER JOIN #VersionedCodeSets c ON c.FK_Reference_Coding_ID = m.FK_Reference_Coding_ID
+	Units,  
+	RepeatMedicationFlag,
+	CAST(MedicationStartDate AS DATE) AS MedicationStartDate,
+	CAST(MedicationEndDate AS DATE) AS MedicationEndDate
+FROM RLS.vw_GP_Medications
 WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients2)
-AND MedicationDate >= @StartDate;
+AND MedicationDate >= DATEADD(year, -1, @StartDate);
 
--- AND (
---   m.FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept IN ('hydroxychloroquine','leflunomide','methotrexate','prednisolone-oral','prednisone','sulfasalazine') AND [Version]=1) OR
---   m.FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept IN ('hydroxychloroquine','leflunomide','methotrexate','prednisolone-oral','prednisone','sulfasalazine') AND [Version]=1)
--- );
