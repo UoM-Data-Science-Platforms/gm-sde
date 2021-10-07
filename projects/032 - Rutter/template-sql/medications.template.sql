3aeb720bab3d4ab24663f38f0c1596ff4d2f1b4b
George Tilston <george.tilston@manchester.ac.uk>
2021-10-07 10:16:19 +0100

    updates to RQ32 template files

---
+++ projects/032 - Rutter/template-sql/medications.template.sql
@@ -0,0 +1,147 @@
+--┌─────────────┐
+--│ Medications │
+--└─────────────┘
+
+------------ RESEARCH DATA ENGINEER CHECK ------------
+-- RICHARD WILLIAMS |	DATE: 20/07/21
+
+-- All prescriptions of: antipsychotic medication.
+
+-- OUTPUT: Data with the following fields
+-- 	-   PatientId (int)
+--	-	MedicationDescription
+--	-	PrescriptionDate (YYYY-MM-DD)
+
+-- Set the start date
+DECLARE @StartDate datetime;
+SET @StartDate = '2019-07-19';
+
+--Just want the output, not the messages
+SET NOCOUNT ON;
+
+-- Find all patients alive at start date
+IF OBJECT_ID('tempdb..#PossiblePatients') IS NOT NULL DROP TABLE #PossiblePatients;
+SELECT PK_Patient_Link_ID as FK_Patient_Link_ID, EthnicMainGroup, DeathDate INTO #PossiblePatients FROM [RLS].vw_Patient_Link
+WHERE (DeathDate IS NULL OR DeathDate >= @StartDate);
+
+-- Find all patients registered with a GP
+IF OBJECT_ID('tempdb..#PatientsWithGP') IS NOT NULL DROP TABLE #PatientsWithGP;
+SELECT DISTINCT FK_Patient_Link_ID INTO #PatientsWithGP FROM [RLS].vw_Patient
+where FK_Reference_Tenancy_ID = 2;
+
+-- Make cohort from patients alive at start date and registered with a GP
+IF OBJECT_ID('tempdb..#Patients') IS NOT NULL DROP TABLE #Patients;
+SELECT pp.* INTO #Patients FROM #PossiblePatients pp
+INNER JOIN #PatientsWithGP gp on gp.FK_Patient_Link_ID = pp.FK_Patient_Link_ID;
+
+--> CODESET diabetes-type-ii:1 polycystic-ovarian-syndrome:1 gestational-diabetes:1
+
+--> EXECUTE query-patient-sex.sql
+--> EXECUTE query-patient-year-of-birth.sql
+
+-- FIND PATIENTS WITH A DIAGNOSIS OF POLYCYSTIC OVARY SYNDROME OR GESTATIONAL DIABETES, TO EXCLUDE
+
+IF OBJECT_ID('tempdb..#exclusions') IS NOT NULL DROP TABLE #exclusions;
+SELECT DISTINCT gp.FK_Patient_Link_ID
+INTO #exclusions
+FROM [RLS].[vw_GP_Events] gp
+LEFT OUTER JOIN #Patients p ON p.FK_Patient_Link_ID = gp.FK_Patient_Link_ID
+WHERE ((SuppliedCode IN 
+	(SELECT [Code] FROM #AllCodes WHERE [Concept] IN 
+		('polycystic-ovarian-syndrome', 'gestational-diabetes') AND [Version] = 1
+			AND EventDate BETWEEN '2018-07-09' AND '2022-03-31')) 
+    
+---- CREATE TABLE OF ALL PATIENTS THAT HAVE ANY LIFETIME DIAGNOSES OF T2D OF 2019-07-19
+
+IF OBJECT_ID('tempdb..#diabetes2_diagnoses') IS NOT NULL DROP TABLE #diabetes2_diagnoses;
+SELECT gp.FK_Patient_Link_ID, 
+		YearOfBirth, 
+		Sex,
+		EthnicMainGroup,
+		EventDate,
+		SuppliedCode,
+		[diabetes_type_i_Code] = CASE WHEN SuppliedCode IN 
+					( SELECT [Code] FROM #AllCodes WHERE [Concept] IN ('diabetes-type-i') AND [Version] = 1 ) THEN 1 ELSE 0 END,
+		[diabetes_type_ii_Code] = CASE WHEN SuppliedCode IN 
+					( SELECT [Code] FROM #AllCodes WHERE [Concept] IN ('diabetes-type-ii') AND [Version] = 1 ) THEN 1 ELSE 0 END
+
+INTO #diabetes2_diagnoses
+FROM [RLS].[vw_GP_Events] gp
+LEFT OUTER JOIN #Patients p ON p.FK_Patient_Link_ID = gp.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientYearOfBirth yob ON yob.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientSex sex ON sex.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientIMDDecile imd ON imd.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+WHERE ((SuppliedCode IN 
+	(SELECT [Code] FROM #AllCodes WHERE [Concept] IN ('diabetes-type-ii') AND [Version] = 1)) 
+    AND gp.FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+    AND gp.FK_Patient_Link_ID NOT IN (SELECT FK_Patient_Link_ID FROM #exclusions) -- exclude patients with polycystic ovary syndrome or gestational diabetes
+	AND (gp.EventDate) <= '2019-07-19'
+
+
+-- Define the main cohort to be matched
+IF OBJECT_ID('tempdb..#MainCohort') IS NOT NULL DROP TABLE #MainCohort;
+SELECT DISTINCT FK_Patient_Link_ID, 
+		YearOfBirth, -- NEED TO ENSURE OVER 18S ONLY AT SOME POINT
+		Sex,
+		EthnicMainGroup,
+		IMD2019Decile1IsMostDeprived10IsLeastDeprived
+INTO #MainCohort
+FROM #diabetes2_diagnoses
+WHERE FK_Patient_Link_ID IN (#####INTERVENTION_TABLE) -- only get patients that had a diabetes intervention
+
+-- Define the population of potential matches for the cohort
+IF OBJECT_ID('tempdb..#PotentialMatches') IS NOT NULL DROP TABLE #PotentialMatches;
+SELECT DISTINCT p.FK_Patient_Link_ID, Sex, YearOfBirth
+INTO #PotentialMatches
+FROM #diabetes2_diagnoses
+WHERE p.FK_Patient_Link_ID NOT IN (SELECT FK_Patient_Link_ID FROM #MainCohort)
+
+
+--> EXECUTE query-cohort-matching-yob-sex-alt.sql yob-flex:1 num-matches:20
+
+
+-- Get the matched cohort detail - same as main cohort
+IF OBJECT_ID('tempdb..#MatchedCohort') IS NOT NULL DROP TABLE #MatchedCohort;
+SELECT 
+  c.MatchingPatientId AS FK_Patient_Link_ID,
+  Sex,
+  MatchingYearOfBirth,
+  EthnicMainGroup,
+  PatientId AS PatientWhoIsMatched
+INTO #MatchedCohort
+FROM #CohortStore c
+LEFT OUTER JOIN #Patients p ON p.FK_Patient_Link_ID = c.MatchingPatientId
+LEFT OUTER JOIN #PatientIMDDecile imd ON imd.FK_Patient_Link_ID = c.MatchingPatientId
+WHERE c.PatientId IN (SELECT FK_Patient_Link_ID FROM #Patients);
+
+-- Define a table with all the patient ids for the main cohort and the matched cohort
+IF OBJECT_ID('tempdb..#PatientIds') IS NOT NULL DROP TABLE #PatientIds;
+SELECT PatientId AS FK_Patient_Link_ID INTO #PatientIds FROM #CohortStore
+UNION
+SELECT MatchingPatientId FROM #CohortStore;
+
+-- RX OF MEDS SINCE 31.07.19 FOR PATIENTS WITH T2D
+
+IF OBJECT_ID('tempdb..#meds') IS NOT NULL DROP TABLE #meds;
+SELECT 
+	 m.FK_Patient_Link_ID,
+	 CAST(MedicationDate AS DATE) as PrescriptionDate,
+	 [description] = CASE WHEN s.[description] IS NOT NULL THEN s.[description] ELSE c.[description] END
+INTO #meds
+FROM RLS.vw_GP_Medications m
+LEFT OUTER JOIN #VersionedSnomedSets s ON s.FK_Reference_SnomedCT_ID = m.FK_Reference_SnomedCT_ID
+LEFT OUTER JOIN #VersionedCodeSets c ON c.FK_Reference_Coding_ID = m.FK_Reference_Coding_ID
+WHERE m.FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #diabetes2_diagnoses)
+AND m.MedicationDate > '2019-07-31' 
+
+-- Produce final table of all medication prescriptions for T2D patients (intervention and controls)
+
+SELECT PatientId, 
+	[description], 
+	PrescriptionDate
+FROM #meds
+ORDER BY PatientId,
+	[description],
+	PrescriptionDate
+
+
