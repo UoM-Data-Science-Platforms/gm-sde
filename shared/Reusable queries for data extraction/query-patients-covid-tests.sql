cdd6fd1fa266e9c14c06088c003ae29624bb6c9e
Iliada Eleftheriou <iliada.eleftheriou@manchester.ac.uk>
2021-12-09 13:29:32 +0000

    modify to return a new temp table with all covid tests, the date, and the clinical code, including positive, negative, excluded, and suspected.

--- shared/Reusable queries for data extraction/query-patients-covid-tests.sql
+++ shared/Reusable queries for data extraction/query-patients-covid-tests.sql
@@ -8,18 +8,23 @@
 -- INPUT: Takes one parameter
 --  - start-date: string - (YYYY-MM-DD) the date to count diagnoses from. Usually this should be 2020-01-01.
 
--- OUTPUT: Two temp tables as follows:
+-- OUTPUT: Three temp tables as follows:
 -- #CovidPositiveTests (FK_Patient_Link_ID, CovidTestDate, CovidTestResult)
 -- #CovidNegativeTests (FK_Patient_Link_ID, CovidTestDate, CovidTestResult)
 -- 	- FK_Patient_Link_ID - unique patient id
 --  - CovidTestDate - Date, assume that only 1 test per day
 --	- CovidTestResult - Varchar, 'Positive', 'Negative'.
+-- #AllCovidTests (FK_Patient_Link_ID, CovidTestDate, CovidTestDescription, ClinicalCode)
+-- 	- FK_Patient_Link_ID - unique patient id
+--  - CovidTestDate - Date, assume that only 1 test per day
+--	- CovidTestResult - Varchar, This field concatenates the information in the GroupDescription and SubGroupDescription from the vw_COVID19 table.
+--  - ClinicalCode - The clinical code retrieved from 'MainCode' in the vw_COVID19 table. 
+
 
 IF OBJECT_ID('tempdb..#CovidPositiveTests') IS NOT NULL DROP TABLE #CovidPositiveTests;
 SELECT DISTINCT FK_Patient_Link_ID, CONVERT(DATE, [EventDate]) AS CovidTestDate, 'Positive' AS CovidTestResult INTO #CovidPositiveTests
 FROM [RLS].[vw_COVID19]
 WHERE (
-	-- TODO: Verify that following condition is the right one.
 	(GroupDescription = 'Confirmed' AND SubGroupDescription != 'Negative') OR
 	(GroupDescription = 'Tested' AND SubGroupDescription = 'Positive')
 )
@@ -30,10 +35,25 @@ IF OBJECT_ID('tempdb..#CovidNegativeTests') IS NOT NULL DROP TABLE #CovidNegativ
 SELECT DISTINCT FK_Patient_Link_ID, CONVERT(DATE, [EventDate]) AS CovidTestDate, 'Negative' AS CovidTestResult INTO #CovidNegativeTests
 FROM [RLS].[vw_COVID19]
 WHERE (
-	-- TODO: Verify that following conditions are the right ones.
 	(GroupDescription = 'Confirmed' AND SubGroupDescription = 'Negative') OR
 	(GroupDescription = 'Tested' AND SubGroupDescription = 'Negative') OR
 	(GroupDescription = 'Excluded' AND SubGroupDescription = 'Negative') 
+)
+AND EventDate >= '{param:start-date}'
+AND EventDate <= GETDATE();
+
+
+-- Get all covid tests, the date, and the clinical code. This includes positive, negative, excluded, suspected. 
+IF OBJECT_ID('tempdb..#AllCovidTests') IS NOT NULL DROP TABLE #AllCovidTests;
+SELECT DISTINCT FK_Patient_Link_ID, CONVERT(DATE, [EventDate]) AS CovidTestDate, CONCAT(GroupDescription, ' - ', SubGroupDescription)  AS CovidTestResult, MainCode AS ClinicalCode INTO #AllCovidTests
+FROM [RLS].[vw_COVID19]
+WHERE (
+	GroupDescription = 'Assessed'  OR
+	GroupDescription = 'Confirmed'  OR
+	GroupDescription = 'Tested'  OR
+	GroupDescription = 'Excluded' OR
+	GroupDescription = 'Suspected'  OR
+	GroupDescription = 'Unknown' 
 )
 AND EventDate >= '{param:start-date}'
 AND EventDate <= GETDATE();
