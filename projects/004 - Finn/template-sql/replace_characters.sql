c3227b833c2c8d027b20c19ef24de0c1875dabdf
Iliada Eleftheriou <iliada.eleftheriou@manchester.ac.uk>
2021-12-09 13:33:02 +0000

    delete as no longer needed. data are now extracted with csv-stringify library so no need to manually escape chars

--- projects/004 - Finn/template-sql/replace_characters.sql
+++
@@ -1,8 +0,0 @@
-REPLACE(
-    REPLACE(
-        REPLACE(
-            REPLACE(
-                REPLACE(
-                    REPLACE(
-                        REPLACE(
-                            [pvt].[ACE comorbidities], CHAR(13), ' '), CHAR(10), '| '), '</p>', '|'), '<p>', ''), '</strong>', ''), '<strong>', ''), '&nbsp;', ' ') AS [ACE comorbidities]
