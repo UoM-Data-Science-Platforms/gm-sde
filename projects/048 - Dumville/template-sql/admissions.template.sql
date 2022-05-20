8e915bad062af11b9ee4ee55c24522d0f9b174d4
Richard Williams <richard.williams2@manchester.ac.uk>
2022-05-20 14:50:53 +0100

    Added code for 048 project

---
+++ projects/048 - Dumville/template-sql/admissions.template.sql
@@ -0,0 +1,32 @@
+--┌────────────┐
+--│ Admissions │
+--└────────────┘
+
+-- OUTPUT: Data with the following fields
+--  - PatientId (int)
+--  - AdmissionDate (YYYYMMDD)
+--  - DischargeDate (YYYYMMDD)
+--  - Status (planned/unplanned/maternity/transfer/other)
+
+--Just want the output, not the messages
+SET NOCOUNT ON;
+
+-- Assume temp table #OxAtHome (FK_Patient_Link_ID, AdmissionDate, DischargeDate)
+
+-- Table of all patients (not matching cohort - will do that subsequently)
+IF OBJECT_ID('tempdb..#Patients') IS NOT NULL DROP TABLE #Patients;
+SELECT FK_Patient_Link_ID INTO #Patients FROM #OxAtHome;
+
+--> EXECUTE query-classify-secondary-admissions.sql
+--> EXECUTE query-get-admissions-and-length-of-stay.sql all-patients:false
+
+SELECT 
+  o.FK_Patient_Link_ID AS PatientId,
+  admit.AdmissionDate,
+  los.DischargeDate,
+  admit.AdmissionType
+FROM #OxAtHome o
+LEFT OUTER JOIN #AdmissionTypes admit ON admit.FK_Patient_Link_ID = o.FK_Patient_Link_ID
+LEFT OUTER JOIN #LengthOfStay los 
+  ON los.FK_Patient_Link_ID = o.FK_Patient_Link_ID
+  AND los.AdmissionDate = admit.AdmissionDate;
