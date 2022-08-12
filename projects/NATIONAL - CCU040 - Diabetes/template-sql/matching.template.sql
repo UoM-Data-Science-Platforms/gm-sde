cd17a7d936d22e1986e3866d877aa3f4a71f4225
Richard Williams <1234richardwilliams@gmail.com>
2022-08-12 16:40:52 +0100

    Update

--- projects/NATIONAL - CCU040 - Diabetes/template-sql/matching.template.sql
+++ projects/NATIONAL - CCU040 - Diabetes/template-sql/matching.template.sql
@@ -25,77 +25,100 @@ TODO
 | PatientId | string | Unique patient id |
 | LSOA      | string | The patients LSOA |
 
--- First let's crystallise the views into actual tables to improve query execution
-CREATE TABLE dars_nic_391419_j3w9t_collab.CCU040_MainCohort
-AS
-SELECT * FROM global_temp.CCU040_MainCohort;
-
-DROP TABLE IF EXISTS dars_nic_391419_j3w9t_collab.CCU040_MainCohort_Male;
-CREATE TABLE dars_nic_391419_j3w9t_collab.CCU040_MainCohort_Male
-AS
-SELECT PatientId, YearOfBirth, FirstCovidPositiveDate FROM dars_nic_391419_j3w9t_collab.CCU040_MainCohort
-WHERE Sex = 1;
-
-DROP TABLE IF EXISTS dars_nic_391419_j3w9t_collab.CCU040_MainCohort_Female;
-CREATE TABLE dars_nic_391419_j3w9t_collab.CCU040_MainCohort_Female
-AS
-SELECT PatientId, YearOfBirth, FirstCovidPositiveDate FROM dars_nic_391419_j3w9t_collab.CCU040_MainCohort
-WHERE Sex = 2;
-
-CREATE TABLE dars_nic_391419_j3w9t_collab.CCU040_PotentialMatches_Male
-AS
-SELECT PatientId, YearOfBirth, FirstCovidPositiveDate FROM global_temp.CCU040_PotentialMatches
-WHERE Sex = 1;
-
 -- Table to store the matches
-CREATE TABLE IF NOT EXISTS dars_nic_391419_j3w9t_collab.CCU040_CohortStore (
+CREATE TABLE IF NOT EXISTS dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store (
   PatientId string, 
   YearOfBirth INT, 
-  Sex nchar(1), 
+  Sex char(1), 
   FirstCovidPositiveDate DATE, 
   MatchingPatientId string,
   MatchingYearOfBirth INT,
   MatchingCovidPositiveDate DATE
 );
-TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_CohortStore;
+TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store;
 
--- 1. If anyone only matches one case then use them. Remove and repeat until everyone matches
---    multiple people or until the CCU040_MainCohort table is empty
-DO $$
-DECLARE LastRowInsert INTEGER :=1; 
-BEGIN  
-  WHILE LastRowInsert > 0 LOOP
-  -- match them
-  INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_CohortStore
-  SELECT PatientId, YearOfBirth, Sex, FirstCovidPositiveDate, MatchedPatientId, YearOfBirth, FirstCovidPositiveDate FROM (
-	  SELECT c.PatientId, c.YearOfBirth, c.Sex, c.FirstCovidPositiveDate, p.PatientId AS MatchedPatientId, Row_Number() OVER(PARTITION BY MAX(c.PatientId) ORDER BY gen_random_uuid()) AS AssignedPersonNumber
-	  FROM dars_nic_391419_j3w9t_collab.CCU040_MainCohort c
-		INNER JOIN dars_nic_391419_j3w9t_collab.CCU040_PotentialMatches p 
-		  ON p.Sex = c.Sex 
-		  AND p.YearOfBirth = c.YearOfBirth 
-		  AND p.FirstCovidPositiveDate = c.FirstCovidPositiveDate
-		WHERE p.PatientId in (
-		-- find patients in the matches who only match a single case
-			select m.PatientId
-		  from dars_nic_391419_j3w9t_collab.CCU040_PotentialMatches m 
-		  inner join dars_nic_391419_j3w9t_collab.CCU040_MainCohort c ON m.Sex = c.Sex 
-			  AND m.YearOfBirth = c.YearOfBirth 
-			  AND m.FirstCovidPositiveDate = c.FirstCovidPositiveDate
-			group by m.PatientId
-			having count(*) = 1
-		)
-	  GROUP BY c.PatientId, c.YearOfBirth, c.Sex, c.FirstCovidPositiveDate, p.PatientId
-	) sub
-	WHERE AssignedPersonNumber <= 5
-  ORDER BY PatientId;
-  GET DIAGNOSTICS LastRowInsert = ROW_COUNT;
-  
-  -- remove from cases anyone we've already got n for
-  delete from dars_nic_391419_j3w9t_collab.CCU040_MainCohort where PatientId in (
-  select PatientId FROM dars_nic_391419_j3w9t_collab.CCU040_CohortStore
-  group by PatientId
-  having count(*) >= 5);
+CREATE TABLE IF NOT EXISTS dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp (
+  PatientId string, 
+  Sex char(1), 
+  YearOfBirth INT, 
+  FirstCovidPositiveDate DATE
+);
+TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp;
 
-  -- remove from matches anyone already used
-  DELETE FROM dars_nic_391419_j3w9t_collab.CCU040_PotentialMatches WHERE PatientId IN (SELECT MatchingPatientId FROM dars_nic_391419_j3w9t_collab.CCU040_CohortStore);
-END
+
+
+-- Now the python bit...
+%python
+
+updated = 1
+
+while updated > 0:
+  before = spark.sql(f'''SELECT COUNT(*) FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store''').head()[0]
+  print('Records in store initially: ' + str(before))
+  print('Finding patients in the potential matches who only match a single case...')
+
+  spark.sql(f'''INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store
+    SELECT PatientId, YearOfBirth, Sex, FirstCovidPositiveDate, MatchedPatientId, YearOfBirth, FirstCovidPositiveDate FROM (
+      SELECT c.PatientId, c.YearOfBirth, c.Sex, c.FirstCovidPositiveDate, p.PatientId AS MatchedPatientId, Row_Number() OVER(PARTITION BY MAX(c.PatientId) ORDER BY random()) AS AssignedPersonNumber
+      FROM dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort c
+      INNER JOIN dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches p 
+        ON p.YearOfBirth = c.YearOfBirth
+        AND p.FirstCovidPositiveDate = c.FirstCovidPositiveDate
+      WHERE p.PatientId in (
+      -- find patients in the matches who only match a single case
+        select m.PatientId
+        from dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches m 
+        inner join dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort c 
+          ON m.YearOfBirth = c.YearOfBirth 
+          AND m.FirstCovidPositiveDate = c.FirstCovidPositiveDate
+        group by m.PatientId
+        having count(*) = 1
+      )
+      GROUP BY c.PatientId, c.YearOfBirth, c.Sex, c.FirstCovidPositiveDate, p.PatientId
+    ) sub
+    WHERE AssignedPersonNumber <= 5
+    ORDER BY PatientId''')
+
+  print("Removing from cases anyone we've already got n for...")
+
+  spark.sql(f'''TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp''')
+
+  spark.sql(f'''INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp
+    SELECT * FROM dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort
+    where PatientId in (
+    select PatientId FROM dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort
+    EXCEPT (
+      select PatientId FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store
+      group by PatientId
+      having count(*) >= 5)
+    );''')
+
+  spark.sql(f'''TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort''')
+
+  spark.sql(f'''INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_Main_Cohort
+    SELECT * FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp''')
+
+  print("Removing from matches anyone we've already used...")
+
+  spark.sql(f'''TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp''')
+
+  spark.sql(f'''INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp
+    SELECT * FROM dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches
+    where PatientId in (
+      select PatientId FROM dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches
+      EXCEPT
+      select MatchingPatientId FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store
+    );''')
+
+  spark.sql(f'''TRUNCATE TABLE dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches''')
+
+  spark.sql(f'''INSERT INTO dars_nic_391419_j3w9t_collab.CCU040_Potential_Matches
+    SELECT * FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store_Temp''')
+
+  after = spark.sql(f'''SELECT COUNT(*) FROM dars_nic_391419_j3w9t_collab.CCU040_Cohort_Store''').head()[0]
+  print('Records in store after inserts: ' + str(after))
+
+  updated = after - before
+  print('Records updated: ' + str(updated))
+
+print('done')
