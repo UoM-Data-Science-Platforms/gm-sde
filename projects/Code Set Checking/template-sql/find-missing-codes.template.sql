4af4256e11d9032f18e2dc518996f883f092b6cf
Richard Williams <1234richardwilliams@gmail.com>
2022-10-04 15:23:36 +0100

    Better scripts for finding codes, and concept summaries

--- projects/Code Set Checking/template-sql/find-missing-codes.template.sql
+++ projects/Code Set Checking/template-sql/find-missing-codes.template.sql
@@ -23,7 +23,7 @@
 --Just want the output, not the messages
 SET NOCOUNT ON;
 
---> CODESET autism-spectrum-disorder:1
+--> CODESET insert-concept-here:1
 
 --TODO doing stuff with CTV3 and Term codes - perhaps need to allow the CTV3 codes in 
 --the code sets to have 10 characters - 5 is the equivalent of the root readv2 code
@@ -36,7 +36,9 @@ CREATE TABLE #CodeCheckOutput (
   [Code] [varchar](20) COLLATE Latin1_General_CS_AS NOT NULL,
   [Term] [varchar](20) COLLATE Latin1_General_CS_AS NULL,
   [Description] [varchar](255) NULL,
-  [Iteration] INT
+  [Iteration] INT,
+	[CodeFromWhichThisWasFound] [varchar](20) COLLATE Latin1_General_CS_AS NULL,
+	[TerminologyOfCodeFromWhichThisWasFound] [varchar](20) COLLATE Latin1_General_CS_AS NULL
 )ON [PRIMARY];
 
 -- If we find missing codes based on mappings between terminologies, then those new codes
@@ -59,7 +61,7 @@ BEGIN
 
 		-- Find all FKs for the EMIS codes
 		IF OBJECT_ID('tempdb..#FKsFromEMIS') IS NOT NULL DROP TABLE #FKsFromEMIS;
-		SELECT FK_Reference_Coding_ID AS CodeId
+		SELECT FK_Reference_Coding_ID AS CodeId, LocalCode AS SourceCode, 'EMIS' AS SourceTerminology
 		INTO #FKsFromEMIS
 		FROM SharedCare.Reference_Local_Code
 		WHERE LocalCode IN (SELECT [code] FROM #codesemis)
@@ -67,7 +69,7 @@ BEGIN
 
 		-- Find all FKs for the SNOMED codes
 		IF OBJECT_ID('tempdb..#FKsFromSNOMED') IS NOT NULL DROP TABLE #FKsFromSNOMED;
-		SELECT PK_Reference_Coding_ID AS CodeId
+		SELECT PK_Reference_Coding_ID AS CodeId, SnomedCT_ConceptID AS SourceCode, 'SNOMED' AS SourceTerminology
 		INTO #FKsFromSNOMED
 		FROM SharedCare.Reference_Coding
 		WHERE SnomedCT_ConceptID IN (SELECT [code] FROM #codessnomed)
@@ -75,7 +77,7 @@ BEGIN
 
 		-- Find all FKs for the Readv2 codes
 		IF OBJECT_ID('tempdb..#FKsFromReadv2') IS NOT NULL DROP TABLE #FKsFromReadv2;
-		SELECT PK_Reference_Coding_ID AS CodeId
+		SELECT PK_Reference_Coding_ID AS CodeId, CASE WHEN Term IS NULL THEN MainCode ELSE CONCAT(MainCode, Term) END AS SourceCode, 'Readv2' AS SourceTerminology
 		INTO #FKsFromReadv2
 		FROM SharedCare.Reference_Coding
 		WHERE EXISTS (SELECT * FROM #codesreadv2 WHERE Code = MainCode AND (Term = SharedCare.Reference_Coding.Term OR Term IS NULL))
@@ -84,7 +86,7 @@ BEGIN
 
 		-- Find all FKs for the CTV3 codes
 		IF OBJECT_ID('tempdb..#FKsFromCTV3') IS NOT NULL DROP TABLE #FKsFromCTV3;
-		SELECT PK_Reference_Coding_ID AS CodeId
+		SELECT PK_Reference_Coding_ID AS CodeId, MainCode AS SourceCode, 'CTV3' AS SourceTerminology
 		INTO #FKsFromCTV3
 		FROM SharedCare.Reference_Coding
 		WHERE EXISTS (SELECT * FROM #codesctv3 WHERE Code = MainCode AND (Term = SharedCare.Reference_Coding.Term OR Term IS NULL))
@@ -93,18 +95,18 @@ BEGIN
 
 		-- Bring together to get all FKs
 		IF OBJECT_ID('tempdb..#FKs') IS NOT NULL DROP TABLE #FKs;
-		SELECT CodeId INTO #FKs FROM #FKsFromCTV3
+		SELECT CodeId, SourceCode, SourceTerminology INTO #FKs FROM #FKsFromCTV3
 		UNION
-		SELECT CodeId FROM #FKsFromEMIS
+		SELECT CodeId, SourceCode, SourceTerminology FROM #FKsFromEMIS
 		UNION
-		SELECT CodeId FROM #FKsFromReadv2
+		SELECT CodeId, SourceCode, SourceTerminology FROM #FKsFromReadv2
 		UNION
-		SELECT CodeId FROM #FKsFromSNOMED;
+		SELECT CodeId, SourceCode, SourceTerminology FROM #FKsFromSNOMED;
 
 	-- Now do the same but this time to get the SNOMED foreign keys
 		-- Find all SNOMED FKs for the EMIS codes
 		IF OBJECT_ID('tempdb..#SNOFKsFromEMIS') IS NOT NULL DROP TABLE #SNOFKsFromEMIS;
-		SELECT FK_Reference_SnomedCT_ID AS SnomedId
+		SELECT FK_Reference_SnomedCT_ID AS SnomedId, LocalCode AS SourceCode, 'EMIS' AS SourceTerminology
 		INTO #SNOFKsFromEMIS
 		FROM SharedCare.Reference_Local_Code
 		WHERE LocalCode IN (SELECT [code] FROM #codesemis)
@@ -112,7 +114,7 @@ BEGIN
 
 		-- Find all SNOMED FKs for the SNOMED codes
 		IF OBJECT_ID('tempdb..#SNOFKsFromSNOMED') IS NOT NULL DROP TABLE #SNOFKsFromSNOMED;
-		SELECT PK_Reference_SnomedCT_ID AS SnomedId
+		SELECT PK_Reference_SnomedCT_ID AS SnomedId, ConceptID AS SourceCode, 'SNOMED' AS SourceTerminology
 		INTO #SNOFKsFromSNOMED
 		FROM SharedCare.Reference_SnomedCT
 		WHERE ConceptID IN (SELECT [code] FROM #codessnomed)
@@ -120,7 +122,7 @@ BEGIN
 
 		-- Find all SNOMED FKs for the Readv2 codes
 		IF OBJECT_ID('tempdb..#SNOFKsFromReadv2') IS NOT NULL DROP TABLE #SNOFKsFromReadv2;
-		SELECT FK_Reference_SnomedCT_ID AS SnomedId
+		SELECT FK_Reference_SnomedCT_ID AS SnomedId, CASE WHEN Term IS NULL THEN MainCode ELSE CONCAT(MainCode, Term) END AS SourceCode, 'Readv2' AS SourceTerminology
 		INTO #SNOFKsFromReadv2
 		FROM SharedCare.Reference_Coding
 		WHERE EXISTS (SELECT * FROM #codesreadv2 WHERE Code = MainCode AND (Term = SharedCare.Reference_Coding.Term OR Term IS NULL))
@@ -129,7 +131,7 @@ BEGIN
 
 		-- Find all SNOMED FKs for the CTV3 codes
 		IF OBJECT_ID('tempdb..#SNOFKsFromCTV3') IS NOT NULL DROP TABLE #SNOFKsFromCTV3;
-		SELECT FK_Reference_SnomedCT_ID AS SnomedId
+		SELECT FK_Reference_SnomedCT_ID AS SnomedId, MainCode AS SourceCode, 'CTV3' AS SourceTerminology
 		INTO #SNOFKsFromCTV3
 		FROM SharedCare.Reference_Coding
 		WHERE EXISTS (SELECT * FROM #codesctv3 WHERE Code = MainCode AND (Term = SharedCare.Reference_Coding.Term OR Term IS NULL))
@@ -138,24 +140,25 @@ BEGIN
 
 		-- Bring together to get all SNOMED FKs
 		IF OBJECT_ID('tempdb..#SNOFKs') IS NOT NULL DROP TABLE #SNOFKs;
-		SELECT SnomedId INTO #SNOFKs FROM #SNOFKsFromCTV3
+		SELECT SnomedId, SourceCode, SourceTerminology INTO #SNOFKs FROM #SNOFKsFromCTV3
 		UNION
-		SELECT SnomedId FROM #SNOFKsFromEMIS
+		SELECT SnomedId, SourceCode, SourceTerminology FROM #SNOFKsFromEMIS
 		UNION
-		SELECT SnomedId FROM #SNOFKsFromReadv2
+		SELECT SnomedId, SourceCode, SourceTerminology FROM #SNOFKsFromReadv2
 		UNION
-		SELECT SnomedId FROM #SNOFKsFromSNOMED;
+		SELECT SnomedId, SourceCode, SourceTerminology FROM #SNOFKsFromSNOMED;
 
 	-- Now we find EMIS codes from the foreign keys that we don't already
 	-- have in our code set
 	IF OBJECT_ID('tempdb..#NewEMIS') IS NOT NULL DROP TABLE #NewEMIS;
-	SELECT LocalCode AS Code, LocalCodeDescription AS [Description] INTO #NewEMIS FROM SharedCare.Reference_Local_Code
-	WHERE FK_Reference_Coding_ID IN (SELECT CodeId FROM #FKs)
-	AND LocalCode NOT IN (SELECT Code FROM #codesemis)
+	SELECT LocalCode AS Code, LocalCodeDescription AS [Description], SourceCode, SourceTerminology
+	INTO #NewEMIS FROM SharedCare.Reference_Local_Code rlc
+	INNER JOIN #FKs f ON f.CodeId = rlc.FK_Reference_Coding_ID
+	WHERE LocalCode NOT IN (SELECT Code FROM #codesemis)
 	UNION
-	SELECT LocalCode, LocalCodeDescription FROM SharedCare.Reference_Local_Code
-	WHERE FK_Reference_SnomedCT_ID IN (SELECT SnomedId FROM #SNOFKs)
-	AND LocalCode NOT IN (SELECT Code FROM #codesemis);
+	SELECT LocalCode, LocalCodeDescription, SourceCode, SourceTerminology FROM SharedCare.Reference_Local_Code rlc
+	INNER JOIN #SNOFKs f ON f.SnomedId = rlc.FK_Reference_SnomedCT_ID
+	WHERE LocalCode NOT IN (SELECT Code FROM #codesemis);
 	SELECT @NewInsertions = @NewInsertions + @@ROWCOUNT;
 
 	-- If we found new ones we add them to the #codesemis table so the 
@@ -164,7 +167,7 @@ BEGIN
 	SELECT '',1,Code,null,[Description] FROM #NewEMIS;
 
 	INSERT INTO #CodeCheckOutput
-	SELECT 'EMIS',Code,null,[Description],@Iteration FROM #NewEMIS;
+	SELECT 'EMIS',Code,null,[Description],@Iteration, SourceCode, SourceTerminology FROM #NewEMIS;
 
 	-- Now we find Readv2 codes from the foreign keys that we don't already
 	-- have in our code set
@@ -180,10 +183,10 @@ BEGIN
 			WHEN Term198 IS NOT NULL THEN Term198 COLLATE Latin1_General_CS_AS
 			WHEN Term60 IS NOT NULL THEN Term60 COLLATE Latin1_General_CS_AS
 			WHEN Term30 IS NOT NULL THEN Term30 COLLATE Latin1_General_CS_AS
-		END AS [Description] INTO #NewReadv2
-	FROM SharedCare.Reference_Coding
-	WHERE PK_Reference_Coding_ID IN (SELECT CodeId FROM #FKs)
-	AND CodingType='ReadCodeV2'
+		END AS [Description], SourceCode, SourceTerminology INTO #NewReadv2
+	FROM SharedCare.Reference_Coding rlc
+	INNER JOIN #FKs f ON f.CodeId = rlc.PK_Reference_Coding_ID
+	WHERE CodingType='ReadCodeV2'
 	AND MainCode NOT IN (SELECT Code FROM #codesreadv2)
 	UNION
 	SELECT
@@ -197,10 +200,10 @@ BEGIN
 			WHEN Term198 IS NOT NULL THEN Term198 COLLATE Latin1_General_CS_AS
 			WHEN Term60 IS NOT NULL THEN Term60 COLLATE Latin1_General_CS_AS
 			WHEN Term30 IS NOT NULL THEN Term30 COLLATE Latin1_General_CS_AS
-		END AS [Description]
-	FROM SharedCare.Reference_Coding
-	WHERE FK_Reference_SnomedCT_ID IN (SELECT SnomedId FROM #SNOFKs)
-	AND CodingType='ReadCodeV2'
+		END AS [Description], SourceCode, SourceTerminology
+	FROM SharedCare.Reference_Coding rlc
+	INNER JOIN #SNOFKs f ON f.SnomedId = rlc.FK_Reference_SnomedCT_ID
+	WHERE CodingType='ReadCodeV2'
 	AND MainCode NOT IN (SELECT Code FROM #codesreadv2);
 	SELECT @NewInsertions = @NewInsertions + @@ROWCOUNT;
 
@@ -210,7 +213,7 @@ BEGIN
 	SELECT '',1,Code,null,[Description] FROM #NewReadv2;
 
 	INSERT INTO #CodeCheckOutput
-	SELECT 'Readv2',Code,Term,[Description],@Iteration FROM #NewReadv2;
+	SELECT 'Readv2',Code,Term,[Description],@Iteration, SourceCode, SourceTerminology FROM #NewReadv2;
 
 	-- Now we find CTV3 codes from the foreign keys that we don't already
 	-- have in our code set
@@ -223,10 +226,10 @@ BEGIN
 			WHEN Term198 IS NOT NULL AND Term198!='' THEN Term198 COLLATE Latin1_General_CS_AS
 			WHEN Term60 IS NOT NULL AND Term60!='' THEN Term60 COLLATE Latin1_General_CS_AS
 			WHEN Term30 IS NOT NULL AND Term30!='' THEN Term30 COLLATE Latin1_General_CS_AS
-		END AS [Description] INTO #NewCTV3
-	FROM SharedCare.Reference_Coding
-	WHERE PK_Reference_Coding_ID IN (SELECT CodeId FROM #FKs)
-	AND CodingType='CTV3'
+		END AS [Description], SourceCode, SourceTerminology INTO #NewCTV3
+	FROM SharedCare.Reference_Coding rlc
+	INNER JOIN #FKs f ON f.CodeId = rlc.PK_Reference_Coding_ID
+	WHERE CodingType='CTV3'
 	AND MainCode NOT IN (SELECT Code FROM #codesctv3)
 	UNION
 	SELECT
@@ -237,10 +240,10 @@ BEGIN
 			WHEN Term198 IS NOT NULL AND Term198!='' THEN Term198 COLLATE Latin1_General_CS_AS
 			WHEN Term60 IS NOT NULL AND Term60!='' THEN Term60 COLLATE Latin1_General_CS_AS
 			WHEN Term30 IS NOT NULL AND Term30!='' THEN Term30 COLLATE Latin1_General_CS_AS
-		END AS [Description]
-	FROM SharedCare.Reference_Coding
-	WHERE FK_Reference_SnomedCT_ID IN (SELECT SnomedId FROM #SNOFKs)
-	AND CodingType='CTV3'
+		END AS [Description], SourceCode, SourceTerminology
+	FROM SharedCare.Reference_Coding rlc
+	INNER JOIN #SNOFKs f ON f.SnomedId = rlc.FK_Reference_SnomedCT_ID
+	WHERE CodingType='CTV3'
 	AND MainCode NOT IN (SELECT Code FROM #codesctv3);
 	SELECT @NewInsertions = @NewInsertions + @@ROWCOUNT;
 
@@ -250,14 +253,15 @@ BEGIN
 	SELECT '',1,Code,Term,[Description] FROM #NewCTV3;
 
 	INSERT INTO #CodeCheckOutput
-	SELECT 'CTV3',Code,Term,[Description],@Iteration FROM #NewCTV3;
+	SELECT 'CTV3',Code,Term,[Description],@Iteration, SourceCode, SourceTerminology FROM #NewCTV3;
 
 	-- Now we find SNOMED codes from the foreign keys that we don't already
 	-- have in our code set
 	IF OBJECT_ID('tempdb..#NewSNOMED') IS NOT NULL DROP TABLE #NewSNOMED;
-	SELECT ConceptID AS Code, Term AS [Description] INTO #NewSNOMED FROM SharedCare.Reference_SnomedCT
-	WHERE PK_Reference_SnomedCT_ID IN (SELECT SnomedId FROM #SNOFKs)
-	AND ConceptID NOT IN (SELECT Code FROM #codessnomed);
+	SELECT ConceptID AS Code, Term AS [Description], SourceCode, SourceTerminology
+	INTO #NewSNOMED FROM SharedCare.Reference_SnomedCT rlc
+	INNER JOIN #SNOFKs f ON f.SnomedId = rlc.PK_Reference_SnomedCT_ID
+	WHERE ConceptID NOT IN (SELECT Code FROM #codessnomed);
 	SELECT @NewInsertions = @NewInsertions + @@ROWCOUNT;
 
 
@@ -267,7 +271,7 @@ BEGIN
 	SELECT '',1,Code,null,[Description] FROM #NewSNOMED;
 
 	INSERT INTO #CodeCheckOutput
-	SELECT 'SNOMED',Code,null,[Description],@Iteration FROM #NewSNOMED;
+	SELECT 'SNOMED',Code,null,[Description],@Iteration, SourceCode, SourceTerminology FROM #NewSNOMED;
 END
 
 -- If NewInsertions is not 0 then it means the iteration hasn't finished yet
@@ -275,5 +279,13 @@ IF @NewInsertions > 0
 SELECT 'More than 5 iterations were performed. Please read the notes at the top of this file.';
 
 -- Final output
-SELECT * FROM #CodeCheckOutput
+SELECT 
+	Terminology, 
+	Code,
+	Term,
+	[Description],
+	Iteration,
+	STRING_AGG(CONCAT(CodeFromWhichThisWasFound, '(',TerminologyOfCodeFromWhichThisWasFound,')'),', ') WITHIN GROUP (ORDER BY CodeFromWhichThisWasFound) AS FoundFrom
+FROM #CodeCheckOutput
+GROUP BY Terminology, Code, Term, Description, Iteration
 ORDER BY Iteration, Terminology, Code, [Description]
--- projects/Code Set Checking/template-sql/summary-of-concept.template.sql
+++ projects/Code Set Checking/template-sql/summary-of-concept.template.sql
@@ -2,29 +2,36 @@
 --│ Provides a summary for a particular code set │
 --└──────────────────────────────────────────────┘
 
--- OBJECTIVE: TODO
+-- OBJECTIVE: To provide a summary of a code set to help determine if there are sufficient
+--						cases for a particular analysis. The intention is that some of the outputs of
+--						this would be provided to the end user who could then decide whether to apply
+--						for a data extract.
 
 -- INPUT: No pre-requisites
 
--- OUTPUT: TODO
+-- OUTPUT:
+--		1) A table showing the prevalence of individual clinical codes
+--		2) A table showing yearly incidence for the concept (how many get it for the first time)
+--		3) A table showing yearly prevalence for the concept (how many people alive with it)
+--		4) A table showing monthly incidence for the concept
+--		5) A table showing monthly prevalence for the concept
 
 --Just want the output, not the messages
 SET NOCOUNT ON;
 
---> CODESET diffuse-large-b-cell-lymphoma:1
-
+--> CODESET insert-concept-here:1
 
 IF OBJECT_ID('tempdb..#Occurrences1') IS NOT NULL DROP TABLE #Occurrences1;
-SELECT FK_Patient_Link_ID, CAST(EventDate AS DATE) AS DateOfConcept
+SELECT FK_Patient_Link_ID, SuppliedCode, FK_Reference_Coding_ID, FK_Reference_SnomedCT_ID, CAST(EventDate AS DATE) AS DateOfConcept
 INTO #Occurrences1
-FROM SharedCare.GP_Events
+FROM RLS.vw_GP_Events
 WHERE FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets)
 AND EventDate IS NOT NULL;
 
 IF OBJECT_ID('tempdb..#Occurrences2') IS NOT NULL DROP TABLE #Occurrences2;
-SELECT FK_Patient_Link_ID, CAST(EventDate AS DATE) AS DateOfConcept
+SELECT FK_Patient_Link_ID, SuppliedCode, FK_Reference_Coding_ID, FK_Reference_SnomedCT_ID, CAST(EventDate AS DATE) AS DateOfConcept
 INTO #Occurrences2
-FROM SharedCare.GP_Events
+FROM RLS.vw_GP_Events
 WHERE FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets)
 AND EventDate IS NOT NULL;
 
@@ -63,6 +70,52 @@ BEGIN
     SET @Year = @Year + 1
 END
 
+IF OBJECT_ID('tempdb..#AllOccurrences') IS NOT NULL DROP TABLE #AllOccurrences;
+SELECT * INTO #AllOccurrences FROM #Occurrences1
+UNION
+SELECT * FROM #Occurrences2;
+
+-- This attempts to show for each clinical code how frequently it occurs.
+IF OBJECT_ID('tempdb..#CodePrevalence') IS NOT NULL DROP TABLE #CodePrevalence;
+SELECT 
+	CASE WHEN Term IS NULL THEN MainCode ELSE CONCAT(MainCode,Term) END AS Code,
+	CodingType,
+	CASE
+		WHEN FullDescription IS NOT NULL AND  FullDescription!='' THEN FullDescription COLLATE Latin1_General_CS_AS
+		WHEN Term198 IS NOT NULL AND  Term198!='' THEN Term198 COLLATE Latin1_General_CS_AS
+		WHEN Term60 IS NOT NULL AND  Term60!='' THEN Term60 COLLATE Latin1_General_CS_AS
+		ELSE Term30 COLLATE Latin1_General_CS_AS
+	END AS Description, Frequency
+INTO #CodePrevalence
+FROM (
+	SELECT 
+		CASE WHEN s1.FK_Reference_Coding_ID IS NULL THEN s2.FK_Reference_Coding_ID ELSE s1.FK_Reference_Coding_ID END AS FK_Reference_Coding_ID,
+		CASE
+			WHEN s1.Frequency IS NULL THEN s2.Frequency
+			WHEN s2.Frequency IS NULL THEN s1.Frequency
+			ELSE s1.Frequency + s2.Frequency
+		END AS Frequency
+	FROM (
+	SELECT FK_Reference_Coding_ID, COUNT(*) AS Frequency FROM #AllOccurrences
+	WHERE FK_Reference_Coding_ID != -1
+	AND SuppliedCode IN (SELECT Code FROM #AllCodes)
+	GROUP BY FK_Reference_Coding_ID ) s1
+	FULL OUTER JOIN  (SELECT FK_Reference_Coding_ID, COUNT(*) AS Frequency FROM #AllOccurrences
+	WHERE FK_Reference_Coding_ID != -1
+	AND SuppliedCode NOT IN (SELECT Code FROM #AllCodes)
+	GROUP BY FK_Reference_Coding_ID ) s2 on s1.FK_Reference_Coding_ID = s2.FK_Reference_Coding_ID
+) sub
+left outer join SharedCare.Reference_Coding r on (r.PK_Reference_Coding_ID = sub.FK_Reference_Coding_ID)
+union
+SELECT ConceptID, 'SNOMED', Term, Frequency FROM (
+	SELECT FK_Reference_SnomedCT_ID, COUNT(*) AS Frequency FROM #AllOccurrences
+	WHERE FK_Reference_Coding_ID = -1
+	AND FK_Reference_SnomedCT_ID != -1
+	GROUP BY FK_Reference_SnomedCT_ID
+) sub
+left outer join SharedCare.Reference_SnomedCT s on (s.PK_Reference_SnomedCT_ID = sub.FK_Reference_SnomedCT_ID)
+order by Frequency desc;
+
 IF OBJECT_ID('tempdb..#FirstOccurrencesYearBreakdown') IS NOT NULL DROP TABLE #FirstOccurrencesYearBreakdown;
 SELECT y.[Year], CASE WHEN Occurrences IS NULL OR Occurrences < 10 THEN '0-9' ELSE CAST(Occurrences AS VARCHAR) END AS Occurrences
 INTO #FirstOccurrencesYearBreakdown
@@ -85,7 +138,7 @@ LEFT OUTER JOIN (
 IF OBJECT_ID('tempdb..#Deaths') IS NOT NULL DROP TABLE #Deaths;
 SELECT PK_Patient_Link_ID AS FK_Patient_Link_ID, DeathDate 
 INTO #Deaths
-FROM SharedCare.Patient_Link
+FROM RLS.vw_Patient_Link
 WHERE PK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #FirstOccurrences);
 
 IF OBJECT_ID('tempdb..#PrevalenceYearBreakdown') IS NOT NULL DROP TABLE #PrevalenceYearBreakdown;
@@ -111,6 +164,9 @@ group by [Year], [Month];
 
 DECLARE @NewLineChar AS CHAR(2) = CHAR(13) + CHAR(10)
 DECLARE @TabChar AS CHAR(1) = CHAR(9);
+
+SELECT * FROM #CodePrevalence
+
 select 'Table 1: The number of people who have this concept in their record for the first time per year (incidence)' + @NewLineChar + @NewLineChar
 union all
 select 'Year' + @TabChar + 'Incidence'
