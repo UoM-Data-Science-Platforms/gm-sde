fb36a295508430f231bc8c65950fd83a0e1dfc4c
Richard Williams <1234richardwilliams@gmail.com>
2022-10-12 16:06:05 +0100

    Added extra logic for deficits from values

--- shared/Reusable queries for data extraction/subquery-efi-common.sql
+++ shared/Reusable queries for data extraction/subquery-efi-common.sql
@@ -72,7 +72,81 @@ CREATE TABLE #EfiEvents (
 --> EXECUTE subquery-efi.sql all-patients:{param:all-patients} gp-events-table:{param:gp-events-table} efi-category:'urinary-incontinence'
 --> EXECUTE subquery-efi.sql all-patients:{param:all-patients} gp-events-table:{param:gp-events-table} efi-category:'urinary-system-disease'
 --> EXECUTE subquery-efi.sql all-patients:{param:all-patients} gp-events-table:{param:gp-events-table} efi-category:'vision-problems'
---> EXECUTE subquery-efi.sql all-patients:{param:all-patients} gp-events-table:{param:gp-events-table} efi-category:'weight-loss '
+--> EXECUTE subquery-efi.sql all-patients:{param:all-patients} gp-events-table:{param:gp-events-table} efi-category:'weight-loss'
+
+-- Now we add some hard coded deficits which come from values rather than
+
+-- First populate a temp table with values for codes of interest
+IF OBJECT_ID('tempdb..#EfiValueData') IS NOT NULL DROP TABLE #EfiValueData;
+BEGIN
+  IF '{param:all-patients}'='true'
+    SELECT FK_Patient_Link_ID, CONVERT(DATE, [EventDate]) AS EventDate, SuppliedCode, [Value]
+		INTO #EfiValueData
+    FROM {param:gp-events-table}
+    WHERE SuppliedCode IN ('16D2.','246V.','246W.','38DE.','39F..','3AD3.','423..','442A.','442W.','44lD.','451E.','451F.','46N..','46N4.','46N7.','46TC.','46W..','585a.','58EE.','66Yf.','687C.','XaJLG','XaF4O','XaF4b','XaP9J','Y1259','Y1258','XaIup','XaK8U','YA310','Y01e7','XaJv3','XE2eH','XE2eG','XaEMS','XE2eI','XE2n3','XE2bw','XSFyN','XaIz7','XaITU','XE2wy','XaELV','39C..','XC0tc','XM0an','XE2m6','Xa96v','Y3351','XaISO','XaZpN','XaK8y','XaMDA','XacUJ','XacUK')
+		AND [Value] IS NOT NULL AND UPPER([Value]) NOT LIKE '%[A-Z]%'  -- EXTRA CHECKS IN CASE ANY NULL OR TEXT VALUES REMAINED
+    AND EventDate <= GETDATE()
+    GROUP BY FK_Patient_Link_ID;
+  ELSE
+    SELECT FK_Patient_Link_ID, CONVERT(DATE, [EventDate]) AS EventDate, SuppliedCode, [Value]
+		INTO #EfiValueData
+    FROM {param:gp-events-table}
+    WHERE SuppliedCode IN ('16D2.','246V.','246W.','38DE.','39F..','3AD3.','423..','442A.','442W.','44lD.','451E.','451F.','46N..','46N4.','46N7.','46TC.','46W..','585a.','58EE.','66Yf.','687C.','XaJLG','XaF4O','XaF4b','XaP9J','Y1259','Y1258','XaIup','XaK8U','YA310','Y01e7','XaJv3','XE2eH','XE2eG','XaEMS','XE2eI','XE2n3','XE2bw','XSFyN','XaIz7','XaITU','XE2wy','XaELV','39C..','XC0tc','XM0an','XE2m6','Xa96v','Y3351','XaISO','XaZpN','XaK8y','XaMDA','XacUJ','XacUK')
+		AND [Value] IS NOT NULL AND UPPER([Value]) NOT LIKE '%[A-Z]%'  -- EXTRA CHECKS IN CASE ANY NULL OR TEXT VALUES REMAINED
+    AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+    AND EventDate <= GETDATE()
+    GROUP BY FK_Patient_Link_ID;
+  END
+END
+
+-- FALLS included if the "number of falls in last 12 months" is >0
+--> EXECUTE subquery-efi-values.sql efi-category:'falls' supplied-codes:"'Y3351','XaISO','16D2.'" min-value:0 max-value:10000 all-patients:{param:all-patients}
+
+-- HYPERTENSION included if avg 24hr diastolic >85
+--> EXECUTE subquery-efi-values.sql efi-category:'hypertension' supplied-codes:"'246V.','XaF4b'" min-value:85 max-value:10000 all-patients:{param:all-patients}
+
+-- HYPERTENSION included if avg 24hr systolic >135
+--> EXECUTE subquery-efi-values.sql efi-category:'hypertension' supplied-codes:"'246W.','XaF4O'" min-value:135 max-value:10000 all-patients:{param:all-patients}
+
+-- AF included if any score
+--> EXECUTE subquery-efi-values.sql efi-category:'atrial-fibrillation' supplied-codes:"'38DE.','XaP9J'" min-value:-1 max-value:10000 all-patients:{param:all-patients}
+
+-- ACTIVITY LIMITATION if Barthel<=18 then deficit
+--> EXECUTE subquery-efi-values.sql efi-category:'activity-limitation' supplied-codes:"'39F..','XM0an'" min-value:-1 max-value:18.1 all-patients:{param:all-patients}
+
+-- Memory & cognitive problems if Six item cognitive impairment test >=8
+--> EXECUTE subquery-efi-values.sql efi-category:'cognitive-problems' supplied-codes:"'3AD3.','XaJLG'" min-value:7.9 max-value:10000 all-patients:{param:all-patients}
+
+-- Anaemia if haemaglobin is below reference range. BUT lower range is 130 for males and I've regularly had less than this
+-- after giving blood so not sure it's a good idea to include.
+--//> EXECUTE subquery-efi-values.sql efi-category:'anaemia' supplied-codes:"'423..','XE2m6','Xa96v'" min-value:X max-value:Y all-patients:{param:all-patients}
+
+-- Thyroid problems if TSH outside of 0.27 - 4.2
+--> EXECUTE subquery-efi-values.sql efi-category:'thyroid-disorders' supplied-codes:"'442A.','442W.','XE2wy','XaELV'" min-value:0 max-value:2.7 all-patients:{param:all-patients}
+--> EXECUTE subquery-efi-values.sql efi-category:'thyroid-disorders' supplied-codes:"'442A.','442W.','XE2wy','XaELV'" min-value:4.2 max-value:10000 all-patients:{param:all-patients}
+
+-- Chronic kidney disease if Glomerular filtration rate <60
+--> EXECUTE subquery-efi-values.sql efi-category:'ckd' supplied-codes:"'451E.','451F.','XSFyN','XaZpN','XaK8y','XaMDA','XacUJ','XacUK'" min-value:0 max-value:60 all-patients:{param:all-patients}
+
+-- Chronic kidney disease if Urine protein
+-- Chronic kidney disease if Urine albumin
+-- Chronic kidney disease if Urine protein/creatinine index
+-- Chronic kidney disease if Urine albumin:creatinine ratio
+-- Chronic kidney disease if Urine microalbumin
+
+-- Peripheral vascular disease if ABPI < 0.95
+--> EXECUTE subquery-efi-values.sql efi-category:'pvd' supplied-codes:"'585a.','Y1259','Y1258','XaIup'" min-value:0 max-value:0.95 all-patients:{param:all-patients}
+
+-- Osteoporosis if Hip DXA scan T score <= -2.5
+--> EXECUTE subquery-efi-values.sql efi-category:'osteoporosis' supplied-codes:"'XaITU','58EE.'" min-value:-10000 max-value:-2.499 all-patients:{param:all-patients}
+
+-- Respiratory problems if Number of COPD exacerbations in past year OR Number of hours of oxygen therapy per day OR
+-- Number of unscheduled encounters for COPD in the last 12 months >= 1
+--> EXECUTE subquery-efi-values.sql efi-category:'respiratory-disease' supplied-codes:"'66Yf.','XaK8U','YA310','Y01e7'" min-value:0.9 max-value:10000 all-patients:{param:all-patients}
+
+-- Weight loss/anorexia if Malnutrition universal screening tool score >= 1
+--> EXECUTE subquery-efi-values.sql efi-category:'weight-loss' supplied-codes:"'687C.','XaJv3'" min-value:0.9999 max-value:10000 all-patients:{param:all-patients}
+
 
 -- Now we need to calculate polypharmacy as that is the 36th EFI deficit
 
@@ -82,9 +156,9 @@ CREATE TABLE #EfiEvents (
 -- Overlapping periods are then combined.
 
 -- Get all the dates that people were prescribed 5 or more meds
-IF OBJECT_ID('tempdb..#PolypharmDates') IS NOT NULL DROP TABLE #PolypharmDates;
+IF OBJECT_ID('tempdb..#PolypharmDates5OnOneDay') IS NOT NULL DROP TABLE #PolypharmDates5OnOneDay;
 SELECT FK_Patient_Link_ID, CONVERT(DATE, [MedicationDate]) AS MedicationDate
-INTO #PolypharmDates
+INTO #PolypharmDates5OnOneDay
 FROM {param:gp-medications-table}
 GROUP BY FK_Patient_Link_ID, CONVERT(DATE, [MedicationDate])
 HAVING COUNT(DISTINCT SuppliedCode) >= 5;
@@ -103,7 +177,7 @@ CREATE TABLE #PolypharmacyPeriodsTEMP (
 IF OBJECT_ID('tempdb..#PolypharmacyPeriods') IS NOT NULL DROP TABLE #PolypharmacyPeriods;
 SELECT FK_Patient_Link_ID, MedicationDate As DateFrom, DATEADD(day, 42, MedicationDate) AS DateTo
 INTO #PolypharmacyPeriods
-FROM PolypharmDates;
+FROM #PolypharmDates5OnOneDay;
 
 DECLARE @NumberDeleted INT;
 SET @NumberDeleted=1;
@@ -167,6 +241,142 @@ BEGIN
 	JOIN (
 	SELECT p1.* FROM #PolypharmacyPeriodsTEMP p1
 	INNER JOIN #PolypharmacyPeriodsTEMP p2 ON
+		p1.FK_Patient_Link_ID = p2.FK_Patient_Link_ID AND
+		(
+			(p1.DateFrom >= p2.DateFrom AND	p1.DateTo < p2.DateTo) OR
+			(p1.DateFrom > p2.DateFrom AND p1.DateTo <= p2.DateTo)
+		)
+	) sub ON sub.FK_Patient_Link_ID = p.FK_Patient_Link_ID and sub.DateFrom = p.DateFrom and sub.DateTo = p.DateTo;
+
+	SELECT @NumberDeleted=@@ROWCOUNT;
+
+	-- Provided we removed some periods, we need to re-run the loop. For our example, the 
+	-- next iteration will first go from:
+	-- |------------|
+	--      |-------------|
+	--                      |-----|
+	-- to:
+	-- |------------|
+	-- |------------------|
+	--      |-------------|
+	--                      |-----|
+	-- during PHASE 1, then during PHASE 2, 2 periods will be deleted leaving:
+	-- |------------------|
+	--                      |-----|
+	-- One more iteration will occur, but nothing will change, so we'll exit the loop with the final
+	-- two non-overlapping periods
+END
+
+
+-- UPDATE Andy Clegg algorithm is for 5 different meds in a 12 month period. Maybe
+-- we should define it in both ways - and allow sensitivity analysis
+-- The following is a look back so gives number of meds in 12 months prior to a prescription
+-- This will deal with the start events.
+IF OBJECT_ID('tempdb..#PolypharmDates5InLastYear') IS NOT NULL DROP TABLE #PolypharmDates5InLastYear;
+SELECT m1.FK_Patient_Link_ID, CONVERT(DATE, m1.[MedicationDate]) AS PotentialPolypharmStartDate
+INTO #PolypharmDates5InLastYear
+FROM {param:gp-medications-table} m1
+LEFT OUTER JOIN {param:gp-medications-table} m2
+	ON m1.FK_Patient_Link_ID = m2.FK_Patient_Link_ID
+	AND CONVERT(DATE, m1.[MedicationDate]) >= CONVERT(DATE, m2.[MedicationDate])
+	AND CONVERT(DATE, m1.[MedicationDate]) < DATEADD(year, 1, CONVERT(DATE, m2.[MedicationDate]))
+GROUP BY m1.FK_Patient_Link_ID, CONVERT(DATE, m1.[MedicationDate])
+HAVING COUNT(DISTINCT m2.SuppliedCode) >= 5;
+
+-- Next is a look forward from the day after a medication. This will deal with the stop events
+IF OBJECT_ID('tempdb..#PolypharmDates5InLastYear') IS NOT NULL DROP TABLE #PolypharmDates5InLastYear;
+SELECT m1.FK_Patient_Link_ID, DATEADD(year, 1, CONVERT(DATE, m1.[MedicationDate])) AS PotentialPolypharmEndDate
+INTO #PolypharmStopDates5InLastYear
+FROM {param:gp-medications-table} m1
+LEFT OUTER JOIN {param:gp-medications-table} m2
+	ON m1.FK_Patient_Link_ID = m2.FK_Patient_Link_ID
+	AND DATEADD(year, 1, CONVERT(DATE, m1.[MedicationDate])) >= CONVERT(DATE, m2.[MedicationDate])
+	AND DATEADD(year, 1, CONVERT(DATE, m1.[MedicationDate])) < DATEADD(year, 1, CONVERT(DATE, m2.[MedicationDate]))
+GROUP BY m1.FK_Patient_Link_ID, DATEADD(year, 1, CONVERT(DATE, m1.[MedicationDate]))
+HAVING COUNT(DISTINCT m2.SuppliedCode) < 5;
+
+-- Now convert to desired format (PatientId / DateFrom / DateTo)
+
+-- Temp holiding table for loop below
+IF OBJECT_ID('tempdb..#PolypharmacyPeriodsYearTEMP') IS NOT NULL DROP TABLE #PolypharmacyPeriodsYearTEMP;
+CREATE TABLE #PolypharmacyPeriodsYearTEMP (
+	FK_Patient_Link_ID BIGINT,
+	DateFrom DATE,
+	DateTo DATE
+);
+
+-- Populate initial start and end dates
+IF OBJECT_ID('tempdb..#PolypharmacyPeriods5In1Year') IS NOT NULL DROP TABLE #PolypharmacyPeriods5In1Year;
+SELECT a.FK_Patient_Link_ID, PotentialPolypharmStartDate AS DateFrom, MIN(PotentialPolypharmEndDate) AS DateTo
+INTO #PolypharmacyPeriods5In1Year
+FROM #PolypharmDates5InLastYear a
+LEFT OUTER JOIN #PolypharmStopDates5InLastYear b
+	ON a.FK_Patient_Link_ID = b.FK_Patient_Link_ID
+	AND PotentialPolypharmStartDate < PotentialPolypharmEndDate
+GROUP BY a.FK_Patient_Link_ID, PotentialPolypharmStartDate
+
+DECLARE @NumberDeleted INT;
+SET @NumberDeleted=1;
+WHILE ( @NumberDeleted > 0)
+BEGIN
+
+	-- PHASE 1
+	-- Populate the temp table with overlapping periods. If there is no overlapping period,
+	-- we just retain the initial period. If a period overlaps, then this populate the widest
+	-- [DateFrom, DateTo] range.
+	-- Grapically we go from:
+	-- |------|
+	--     |-----|
+	--      |-------|
+	--              |-----|
+	--                      |-----|
+	-- to:
+	-- |------|
+	-- |---------|
+	-- |------------|
+	--     |-----|
+	--     |--------|
+	--      |-------|
+	--      |-------------|
+	--              |-----|
+	--                      |-----|
+	--
+	TRUNCATE TABLE #PolypharmacyPeriodsYearTEMP;
+	INSERT INTO #PolypharmacyPeriodsYearTEMP
+	select p1.FK_Patient_Link_ID, p1.DateFrom, ISNULL(p2.DateTo, p1.DateTo) AS DateTo
+	from #PolypharmacyPeriods5In1Year p1
+	left outer join #PolypharmacyPeriods5In1Year p2 on
+		p1.FK_Patient_Link_ID = p2.FK_Patient_Link_ID and 
+		p2.DateFrom <= p1.DateTo and 
+		p2.DateFrom > p1.DateFrom;
+
+	-- Make both polypharm period tables the same
+	TRUNCATE TABLE #PolypharmacyPeriods5In1Year;
+	INSERT INTO #PolypharmacyPeriods5In1Year
+	SELECT * FROM #PolypharmacyPeriodsYearTEMP;
+
+	-- PHASE 2
+	-- The above will have resulted in overlapping periods. Here we remove any that are
+	-- contained in other periods.
+	-- Continuing the above graphical example, we go from:
+	-- |------|
+	-- |---------|
+	-- |------------|
+	--     |-----|
+	--     |--------|
+	--      |-------|
+	--      |-------------|
+	--              |-----|
+	--                      |-----|
+	-- to:
+	-- |------------|
+	--      |-------------|
+	--                      |-----|
+	DELETE p
+	FROM #PolypharmacyPeriods5In1Year p
+	JOIN (
+	SELECT p1.* FROM #PolypharmacyPeriodsYearTEMP p1
+	INNER JOIN #PolypharmacyPeriodsYearTEMP p2 ON
 		p1.FK_Patient_Link_ID = p2.FK_Patient_Link_ID AND
 		(
 			(p1.DateFrom >= p2.DateFrom AND	p1.DateTo < p2.DateTo) OR
--- shared/Reusable queries for data extraction/subquery-efi-values.sql
+++ shared/Reusable queries for data extraction/subquery-efi-values.sql
@@ -2,37 +2,36 @@
 --│ Electronic Frailty Index subquery │
 --└───────────────────────────────────┘
 
--- OBJECTIVE: Subquery for the EFI calculation to ensure consistency instead of copy/pasting the same code 35 times.
+-- OBJECTIVE: Subquery for the EFI calculation to ensure consistency. This allows a code with a value
+--            within a range to be counted towards a deficit
 
--- INPUT: Assumes there exists a temp table #EfiEvents(FK_Patient_Link_ID, Deficit, EventDate)
+-- INPUT: Assumes there exists a temp table #EfiEvents(FK_Patient_Link_ID, Deficit, EventDate) and 
+--        a temp table #EfiValueData(FK_Patient_Link_ID, EventDate, SuppliedCode, Value)
 --  ALso takes three parameters
 --  - efi-category: string - e.g. "activity-limitation" to match the "efi-activity-limitation" code set
 --	-	all-patients: boolean - (true/false) if true, then all patients are included, otherwise only those in the pre-existing #Patients table.
---	- gp-events-table: string - (table name) the name of the table containing the GP events. Usually is "RLS.vw_GP_Events" but can be anything with the columns: FK_Patient_Link_ID, EventDate, and SuppliedCode
-
+--	- supplied-codes: string - (of form 'code','code2',...,'coden') the codes to include
+--  - min-value: int - The minimum value to count. Strict inequality so "0" would become >0
+--  - max-value: int - The maximum value to count. Strict inequality so "0" would become <0
 -- OUTPUT: None. This populates the pre-existing #EfiEvents table with the first time the patient experienced the deficit specified by the efi-category.
 
 BEGIN
   IF '{param:all-patients}'='true'
     INSERT INTO #EfiEvents
     SELECT FK_Patient_Link_ID, '{param:efi-category}' AS Deficit, MIN(CONVERT(DATE, [EventDate])) AS EventDate
-    FROM {param:gp-events-table}
-    WHERE SuppliedCode IN (
-      select Code from #AllCodes 
-      where Concept = 'efi-{param:efi-category}'
-      AND Version = 1
-    )
+    FROM #EfiValueData
+    WHERE SuppliedCode IN ({param:supplied-codes})
+		AND [Value] > {param:min-value}
+    AND [Value] < {param:max-value}
     AND EventDate <= GETDATE()
     GROUP BY FK_Patient_Link_ID;
   ELSE
     INSERT INTO #EfiEvents
     SELECT FK_Patient_Link_ID, '{param:efi-category}' AS Deficit, MIN(CONVERT(DATE, [EventDate])) AS EventDate
-    FROM {param:gp-events-table}
-    WHERE SuppliedCode IN (
-      select Code from #AllCodes 
-      where Concept = 'efi-{param:efi-category}'
-      AND Version = 1
-    )
+    FROM #EfiValueData
+    WHERE SuppliedCode IN ({param:supplied-codes})
+		AND [Value] > {param:min-value}
+    AND [Value] < {param:max-value}
     AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
     AND EventDate <= GETDATE()
     GROUP BY FK_Patient_Link_ID;
