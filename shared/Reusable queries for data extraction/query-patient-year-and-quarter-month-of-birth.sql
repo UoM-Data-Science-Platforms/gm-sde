c8cb2114c1cd620fe807a661681b6cc299d523a4
maiparkes58 <lemai.parkes@manchester.ac.uk>
2022-12-07 15:00:37 +0000

    Update typo

---
+++ shared/Reusable queries for data extraction/query-patient-year-and-quarter-month-of-birth.sql
@@ -0,0 +1,92 @@
+﻿--┌────────────────────────────────┐
+--│ Year and quarter month of birth │
+--└────────────────────────────────┘
+
+-- OBJECTIVE: To get the year of birth for each patient.
+
+-- INPUT: Assumes there exists a temp table as follows:
+-- #Patients (FK_Patient_Link_ID)
+--  A distinct list of FK_Patient_Link_IDs for each patient in the cohort
+
+-- OUTPUT: A temp table as follows:
+-- #PatientYearAndQuarterMonthOfBirth (FK_Patient_Link_ID, YearAndQuarterMonthOfBirth)
+-- 	- FK_Patient_Link_ID - unique patient id
+--	- YearAndQuarterMonthOfBirth - (YYYY-MM-01)
+
+-- ASSUMPTIONS:
+--	- Patient data is obtained from multiple sources. Where patients have multiple YearAndQuarterMonthOfBirths we determine the YearAndQuarterMonthOfBirth as follows:
+--	-	If the patients has a YearAndQuarterMonthOfBirth in their primary care data feed we use that as most likely to be up to date
+--	-	If every YearAndQuarterMonthOfBirth for a patient is the same, then we use that
+--	-	If there is a single most recently updated YearAndQuarterMonthOfBirth in the database then we use that
+--	-	Otherwise we take the highest YearAndQuarterMonthOfBirth for the patient that is not in the future
+
+-- Get all patients year and quarter month of birth for the cohort
+IF OBJECT_ID('tempdb..#AllPatientYearAndQuarterMonthOfBirths') IS NOT NULL DROP TABLE #AllPatientYearAndQuarterMonthOfBirths;
+SELECT 
+	FK_Patient_Link_ID,
+	FK_Reference_Tenancy_ID,
+	HDMModifDate,
+	CONVERT(date, Dob) AS YearAndQuarterMonthOfBirth
+INTO #AllPatientYearAndQuarterMonthOfBirths
+FROM SharedCare.Patient p
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+AND Dob IS NOT NULL;
+
+
+-- If patients have a tenancy id of 2 we take this as their most likely YearAndQuarterMonthOfBirth
+-- as this is the GP data feed and so most likely to be up to date
+IF OBJECT_ID('tempdb..#PatientYearAndQuarterMonthOfBirth') IS NOT NULL DROP TABLE #PatientYearAndQuarterMonthOfBirth;
+SELECT FK_Patient_Link_ID, MIN(YearAndQuarterMonthOfBirth) as YearAndQuarterMonthOfBirth INTO #PatientYearAndQuarterMonthOfBirth FROM #AllPatientYearAndQuarterMonthOfBirths
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+AND FK_Reference_Tenancy_ID = 2
+GROUP BY FK_Patient_Link_ID
+HAVING MIN(YearAndQuarterMonthOfBirth) = MAX(YearAndQuarterMonthOfBirth);
+
+-- Find the patients who remain unmatched
+IF OBJECT_ID('tempdb..#UnmatchedYobPatients') IS NOT NULL DROP TABLE #UnmatchedYobPatients;
+SELECT FK_Patient_Link_ID INTO #UnmatchedYobPatients FROM #Patients
+EXCEPT
+SELECT FK_Patient_Link_ID FROM #PatientYearAndQuarterMonthOfBirth;
+
+-- If every YearAndQuarterMonthOfBirth is the same for all their linked patient ids then we use that
+INSERT INTO #PatientYearAndQuarterMonthOfBirth
+SELECT FK_Patient_Link_ID, MIN(YearAndQuarterMonthOfBirth) FROM #AllPatientYearAndQuarterMonthOfBirths
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #UnmatchedYobPatients)
+GROUP BY FK_Patient_Link_ID
+HAVING MIN(YearAndQuarterMonthOfBirth) = MAX(YearAndQuarterMonthOfBirth);
+
+-- Find any still unmatched patients
+TRUNCATE TABLE #UnmatchedYobPatients;
+INSERT INTO #UnmatchedYobPatients
+SELECT FK_Patient_Link_ID FROM #Patients
+EXCEPT
+SELECT FK_Patient_Link_ID FROM #PatientYearAndQuarterMonthOfBirth;
+
+-- If there is a unique most recent YearAndQuarterMonthOfBirth then use that
+INSERT INTO #PatientYearAndQuarterMonthOfBirth
+SELECT p.FK_Patient_Link_ID, MIN(p.YearAndQuarterMonthOfBirth) FROM #AllPatientYearAndQuarterMonthOfBirths p
+INNER JOIN (
+	SELECT FK_Patient_Link_ID, MAX(HDMModifDate) MostRecentDate FROM #AllPatientYearAndQuarterMonthOfBirths
+	WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #UnmatchedYobPatients)
+	GROUP BY FK_Patient_Link_ID
+) sub ON sub.FK_Patient_Link_ID = p.FK_Patient_Link_ID AND sub.MostRecentDate = p.HDMModifDate
+GROUP BY p.FK_Patient_Link_ID
+HAVING MIN(YearAndQuarterMonthOfBirth) = MAX(YearAndQuarterMonthOfBirth);
+
+-- Find any still unmatched patients
+TRUNCATE TABLE #UnmatchedYobPatients;
+INSERT INTO #UnmatchedYobPatients
+SELECT FK_Patient_Link_ID FROM #Patients
+EXCEPT
+SELECT FK_Patient_Link_ID FROM #PatientYearAndQuarterMonthOfBirth;
+
+-- Otherwise just use the highest value (with the exception that can't be in the future)
+INSERT INTO #PatientYearAndQuarterMonthOfBirth
+SELECT FK_Patient_Link_ID, MAX(YearAndQuarterMonthOfBirth) FROM #AllPatientYearAndQuarterMonthOfBirths
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #UnmatchedYobPatients)
+GROUP BY FK_Patient_Link_ID
+HAVING MAX(YearAndQuarterMonthOfBirth) <= GETDATE();
+
+-- Tidy up - helpful in ensuring the tempdb doesn't run out of space mid-query
+DROP TABLE #AllPatientYearAndQuarterMonthOfBirths;
+DROP TABLE #UnmatchedYobPatients;
