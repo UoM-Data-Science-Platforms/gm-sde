d546d01fe947abcd5e15d63ebdd53931af70d6a7
Richard Williams <richard.williams2@manchester.ac.uk>
2023-01-12 11:55:38 +0000

    Final update to EFI queries

--- shared/Reusable queries for data extraction/subquery-efi-values.sql
+++ shared/Reusable queries for data extraction/subquery-efi-values.sql
@@ -8,9 +8,8 @@
 
 -- INPUT: Assumes there exists a temp table #EfiEvents(FK_Patient_Link_ID, Deficit, EventDate) and 
 --        a temp table #EfiValueData(FK_Patient_Link_ID, EventDate, SuppliedCode, Value)
---  ALso takes three parameters
+--  ALso takes parameters
 --  - efi-category: string - e.g. "activity-limitation" to match the "efi-activity-limitation" code set
---	-	all-patients: boolean - (true/false) if false, then only those in the pre-existing #Patients table are included, otherwise everyone.
 --	-	patients: temp table name - OPTIONAL - e.g. "#Patients" allows filtering to just some patients
 --	- supplied-codes: string - (of form 'code','code2',...,'coden') the codes to include
 --  - min-value: int - OPTIONAL - The minimum value to count. Strict inequality so "0" would become >0
@@ -28,11 +27,6 @@ AND TRY_CONVERT(NUMERIC (18,5), [Value]) > {param:min-value}
 {if:max-value}
 AND TRY_CONVERT(NUMERIC (18,5), [Value]) < {param:max-value}
 {endif:max-value}
-{if:patients}
 AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM {param:patients})
-{endif:patients}
-{if:all-patients=false}
-AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
-{endif:all-patients}
 AND EventDate <= GETDATE()
 GROUP BY FK_Patient_Link_ID;
