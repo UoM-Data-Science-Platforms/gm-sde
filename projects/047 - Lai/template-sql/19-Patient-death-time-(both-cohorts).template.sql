88a77b82d0e56e0e3b4985101e356846d239e38f
maiparkes58 <lemai.parkes@manchester.ac.uk>
2023-06-01 15:49:39 +0100

    Add death time for RQ047

---
+++ projects/047 - Lai/template-sql/19-Patient-death-time-(both-cohorts).template.sql
@@ -0,0 +1,65 @@
+﻿--+--------------------------------------------------------------------------------+
+--¦ Patient death time                                                             ¦
+--+--------------------------------------------------------------------------------+
+
+-------- RESEARCH DATA ENGINEER CHECK ---------
+
+-- OUTPUT: Data with the following fields
+-- PatientId
+-- YearAndMonthOfDeath (YYYY-MM)
+
+
+--> CODESET skin-cancer:1
+--> CODESET gynaecological-cancer:1
+
+
+-- Set the start date
+DECLARE @StartDate datetime;
+DECLARE @EndDate datetime;
+SET @StartDate = '2011-01-01';
+SET @EndDate = '2022-06-01';
+
+--Just want the output, not the messages
+SET NOCOUNT ON;
+
+
+-- Create the skin cancer cohort=====================================================================================================================================
+IF OBJECT_ID('tempdb..#SkinCohort') IS NOT NULL DROP TABLE #SkinCohort;
+SELECT DISTINCT FK_Patient_Link_ID 
+INTO #SkinCohort
+FROM SharedCare.GP_Events
+WHERE (
+  FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = 'skin-cancer' AND Version = 1) OR
+  FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = 'skin-cancer' AND Version = 1)
+) AND EventDate >= @StartDate AND EventDate < @EndDate;
+
+
+-- Create the gynae cancer cohort========================================================================================================
+IF OBJECT_ID('tempdb..#GynaeCohort') IS NOT NULL DROP TABLE #GynaeCohort;
+SELECT DISTINCT FK_Patient_Link_ID 
+INTO #GynaeCohort
+FROM SharedCare.GP_Events
+WHERE (
+  FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = 'gynaecological-cancer' AND Version = 1) OR
+  FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = 'gynaecological-cancer' AND Version = 1)
+) AND EventDate >= @StartDate AND EventDate < @EndDate;
+
+
+-- Create a table with all patients for post COPI and within 2 cohorts=========================================================================================================================
+IF OBJECT_ID('tempdb..#PatientsToInclude') IS NOT NULL DROP TABLE #PatientsToInclude;
+SELECT FK_Patient_Link_ID INTO #PatientsToInclude
+FROM [SharedCare].[Patient_GP_History]
+WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #SkinCohort) OR FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #GynaeCohort)
+GROUP BY FK_Patient_Link_ID
+HAVING MIN(StartDate) < '2022-06-01';
+
+IF OBJECT_ID('tempdb..#Patients') IS NOT NULL DROP TABLE #Patients;
+SELECT DISTINCT FK_Patient_Link_ID 
+INTO #Patients 
+FROM #PatientsToInclude;
+
+
+-- Select death time (month and year) for the cohort patients================================================================================================================================
+SELECT p.FK_Patient_Link_ID AS PatientId, FORMAT(l.DeathDate, 'yyyy-MM') AS YearAndMonthOfDeath
+FROM #Patients p
+LEFT OUTER JOIN [SharedCare].[Patient_Link] l ON p.FK_Patient_Link_ID = l.PK_Patient_Link_ID
