41d720813f746c21e01a110c8c34915288a0414a
maiparkes58 <lemai.parkes@manchester.ac.uk>
2023-09-01 14:21:38 +0100

    Create reusable cohort query RQ045

---
+++ shared/Reusable queries for data extraction/query-build-rq045-cohort.sql
@@ -0,0 +1,14 @@
+--┌──────────────────────────────────────────────────────────────────────┐
+--│ Define Cohort for RQ045: COVID-19 vaccine hesitancy and acceptance   │
+--└──────────────────────────────────────────────────────────────────────┘
+
+-- OBJECTIVE: To build the cohort of patients needed for RQ045. This reduces
+--						duplication of code in the template scripts. The cohort is any
+--						patient who ...
+
+-- INPUT: assumes there exists one temp table as follows:
+-- #Patients (FK_Patient_Link_ID)
+--  A distinct list of FK_Patient_Link_IDs for each patient in the cohort
+
+-- OUTPUT: Temp tables as follows:
+-- #Cohort - list of patient ids of the cohort
