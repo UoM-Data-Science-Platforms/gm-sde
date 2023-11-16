e000aa46215dc148c94ae1e916cbce1ff9b75051
Richard Williams <rw251@users.noreply.github.com>
2023-11-16 10:46:58 +0000

    - modified the first diagnosis to improve performance (#148)
    - created an equivalent script to get first prescription
    - added a script to get the most recent test value

--- shared/Reusable queries for data extraction/query-get-first-diagnosis.sql
+++ shared/Reusable queries for data extraction/query-get-first-diagnosis.sql
@@ -12,18 +12,32 @@
 --  - version: number - the code set version
 --  - temp-table-name: string - the name of the temp table that this will produce
 
--- OUTPUT: Temp tables as follows:
--- #Patients - list of patient ids of the cohort
+-- OUTPUT: Temp table with the name of the provided parameter `temp-table-name` with columns:
+--	-	FK_Patient_Link_ID - unique patient id
+--	-	DateOfFirstDiagnosis - date (YYY/MM/DD) - the date of the first diagnosis
 {endif:verbose}
 
 --> CODESET {param:code-set}:{param:version}
+-- We find the first occurrence of the relevant code for each patient. For performance reasons
+-- we first search by FK_Reference_Coding_ID, and then later, separately, for FK_Reference_SnomedCT_ID.
+-- Combining these into an OR statement in a WHERE clause in a single query is substantially slower than
+-- searching for each individually and then combining.
+IF OBJECT_ID('tempdb..{param:temp-table-name}temppart1') IS NOT NULL DROP TABLE {param:temp-table-name}temppart1;
 SELECT FK_Patient_Link_ID, MIN(EventDate) AS DateOfFirstDiagnosis
-INTO {param:temp-table-name}
+INTO {param:temp-table-name}temppart1
 FROM {param:gp-events-table}
-WHERE (
-  FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = '{param:code-set}' AND Version = {param:version}) OR
-  FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = '{param:code-set}' AND Version = {param:version})
-)
+WHERE FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = '{param:code-set}' AND Version = {param:version})
+{if:all-patients=false}
+AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+{endif:all-patients}
+GROUP BY FK_Patient_Link_ID;
+
+-- As per above now we find the first instance of the code based on the FK_Reference_SnomedCT_ID
+IF OBJECT_ID('tempdb..{param:temp-table-name}temppart2') IS NOT NULL DROP TABLE {param:temp-table-name}temppart2;
+SELECT FK_Patient_Link_ID, MIN(EventDate) AS DateOfFirstDiagnosis
+INTO {param:temp-table-name}temppart2
+FROM {param:gp-events-table}
+WHERE FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = '{param:code-set}' AND Version = {param:version})
 {if:patients}
 AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM {param:patients})
 {endif:patients}
@@ -31,3 +45,20 @@ AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM {param:patients})
 AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
 {endif:all-patients}
 GROUP BY FK_Patient_Link_ID;
+
+-- We now join the two tables above. By doing a FULL JOIN we include all records from both tables.
+-- In each row, at least one of the DateOfFirstDiagnosis will be non NULL. Therefore if one field
+-- is NULL, then we use the other. If both are non NULL, then we take the earliest as the goal is
+-- to get the first occurrence of the code for each patient.
+IF OBJECT_ID('tempdb..{param:temp-table-name}') IS NOT NULL DROP TABLE {param:temp-table-name};
+SELECT
+	CASE WHEN p1.FK_Patient_Link_ID IS NULL THEN p2.FK_Patient_Link_ID ELSE p1.FK_Patient_Link_ID END AS FK_Patient_Link_ID,
+	CASE
+		WHEN p1.DateOfFirstDiagnosis IS NULL THEN p2.DateOfFirstDiagnosis
+		WHEN p2.DateOfFirstDiagnosis IS NULL THEN p1.DateOfFirstDiagnosis
+		WHEN p1.DateOfFirstDiagnosis < p2.DateOfFirstDiagnosis THEN p1.DateOfFirstDiagnosis
+		ELSE p2.DateOfFirstDiagnosis
+	END AS DateOfFirstDiagnosis
+INTO {param:temp-table-name}
+FROM {param:temp-table-name}temppart1 p1
+FULL JOIN {param:temp-table-name}temppart2 p2 on p1.FK_Patient_Link_ID = p2.FK_Patient_Link_ID
