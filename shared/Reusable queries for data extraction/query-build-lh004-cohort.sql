7381ab7b3fa974f5766e7933c382ee122e00badd
George Tilston <george.tilston@manchester.ac.uk>
2023-12-20 14:56:51 +0000

    setup lh004 folder and start on scripts

---
+++ shared/Reusable queries for data extraction/query-build-lh004-cohort.sql
@@ -0,0 +1,72 @@
+--┌───────────────────────────────────────────────────────────────┐
+--│ Define Cohort for LH004: patients that had an SLE diagnosis   │
+--└───────────────────────────────────────────────────────────────┘
+
+-- OBJECTIVE: To build the cohort of patients needed for LH003. This reduces duplication of code in the template scripts.
+
+-- COHORT: Any patient with a SLE diagnosis between start and end date.
+
+-- INPUT: assumes there exists one temp table as follows:
+-- #Patients (FK_Patient_Link_ID)
+--  A distinct list of FK_Patient_Link_IDs for each patient in the cohort
+
+-- OUTPUT: Temp tables as follows:
+-- #Cohort
+
+--> EXECUTE query-get-possible-patients.sql
+--> EXECUTE query-patient-year-of-birth.sql
+
+--> CODESET sle:1 
+
+----
+-- TO DO: CODESETS for exclusion conditions ------
+----
+
+-- table of sle coding events
+
+IF OBJECT_ID('tempdb..#SLECodes') IS NOT NULL DROP TABLE #SLECodes;
+SELECT FK_Patient_Link_ID, EventDate, COUNT(*) AS NumberOfSLECodes
+INTO #SLECodes
+FROM SharedCare.[GP_Events]
+WHERE (
+  FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = 'sle' AND Version = 1) OR
+  FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = 'sle' AND Version = 1)
+)
+GROUP BY FK_Patient_Link_ID, EventDate
+
+
+-- table of patients that meet the exclusion criteria: turberculosis, lupus pernio, drug-induced lupus, neonatal lupus
+/*
+IF OBJECT_ID('tempdb..#Exclusions') IS NOT NULL DROP TABLE #Exclusions;
+SELECT FK_Patient_Link_ID AS PatientId, EventDate
+INTO #Exclusions
+FROM SharedCare.[GP_Events]
+WHERE (
+  FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept in () AND Version = 1) OR
+  FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept in () AND Version = 1)
+)
+GROUP BY FK_Patient_Link_ID, EventDate
+*/
+
+
+-- create cohort of patients with an SLE diagnosis in the study period
+
+IF OBJECT_ID('tempdb..#Cohort') IS NOT NULL DROP TABLE #Cohort;
+SELECT
+	 p.FK_Patient_Link_ID
+	,yob.YearOfBirth
+	,p.EthnicGroupDescription
+	,p.DeathDate
+INTO #Cohort
+FROM #Patients p
+LEFT OUTER JOIN #PatientYearOfBirth yob ON yob.FK_Patient_Link_ID = p.FK_Patient_Link_ID
+WHERE 
+	p.FK_Patient_Link_ID IN (SELECT DISTINCT FK_Patient_Link_ID FROM #SLECodes WHERE NumberOfSLECodes >= 1)
+	--AND 
+	--p.FK_Patient_Link_ID NOT IN (SELECT DISTINCT FK_Patient_Link_ID FROM #Exclusions)
+AND YEAR(@StartDate) - YearOfBirth > 18
+
+
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
