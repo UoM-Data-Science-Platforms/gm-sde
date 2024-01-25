8e3cfcf6aa5ecde3d1c10077f05fc7de7694806a
Richard Williams <1234richardwilliams@gmail.com>
2024-01-25 12:02:28 +0000

    Update

--- shared/Reusable queries for data extraction/query-build-rq066-cohort.sql
+++ shared/Reusable queries for data extraction/query-build-rq066-cohort.sql
@@ -21,18 +21,55 @@ FROM SharedCare.Patient
 WHERE FK_Reference_Tenancy_ID=2
 AND GPPracticeCode NOT LIKE 'ZZZ%';
 
+--> EXECUTE query-patients-with-post-covid-syndrome.sql start-date:2020-01-01 gp-events-table:SharedCare.GP_Events all-patients:false
+--> EXECUTE query-patients-with-covid.sql start-date:2020-01-01 all-patients:false gp-events-table:SharedCare.GP_Events
+
 --> EXECUTE query-patient-year-of-birth.sql
+--> EXECUTE query-patient-sex.sql
 
--- Now restrict to those >=18
-TRUNCATE TABLE #Patients;
-INSERT INTO #Patients
-SELECT FK_Patient_Link_ID FROM #PatientYearOfBirth
-WHERE YearOfBirth <= YEAR(GETDATE()) - 18;
+-- Define the main cohort that will be matched
+IF OBJECT_ID('tempdb..#MainCohort') IS NOT NULL DROP TABLE #MainCohort;
+SELECT 
+  c.FK_Patient_Link_ID,
+  FirstCovidPositiveDate AS IndexDate,
+  FirstPostCOVIDDiagnosisDate,
+  FirstPostCOVIDReferralDate,
+  FirstPostCOVIDAssessmentDate
+  Sex,
+  YearOfBirth
+INTO #MainCohort
+FROM #CovidPatients c
+LEFT OUTER JOIN #PatientSex sex ON sex.FK_Patient_Link_ID = c.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientYearOfBirth yob ON yob.FK_Patient_Link_ID = c.FK_Patient_Link_ID
+WHERE c.FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #PostCOVIDPatients);
 
--- NB get-first-diagnosis is fine even though T4 level is not a diagnosis as both codes appear in the Events table
---> EXECUTE query-get-first-diagnosis.sql all-patients:false gp-events-table:SharedCare.GP_Events code-set:t4 version:1 temp-table-name:#FirstT4Level
+-- Define the population of potential matches for the cohort
+IF OBJECT_ID('tempdb..#PotentialMatches') IS NOT NULL DROP TABLE #PotentialMatches;
+SELECT c.FK_Patient_Link_ID, FirstCovidPositiveDate AS IndexDate, Sex, YearOfBirth
+INTO #PotentialMatches
+FROM #CovidPatients c
+LEFT OUTER JOIN #PatientSex sex ON sex.FK_Patient_Link_ID = c.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientYearOfBirth yob ON yob.FK_Patient_Link_ID = c.FK_Patient_Link_ID
+EXCEPT
+SELECT FK_Patient_Link_ID, IndexDate, Sex, YearOfBirth FROM #MainCohort;
 
--- Now restrict patients to just those with a T4 level
-TRUNCATE TABLE #Patients;
-INSERT INTO #Patients
-SELECT FK_Patient_Link_ID FROM #FirstT4Level;
+--> EXECUTE query-cohort-matching-yob-sex-index-date.sql index-date-flex:14 yob-flex:5
+
+-- Get the matched cohort detail - same as main cohort
+IF OBJECT_ID('tempdb..#MatchedCohort') IS NOT NULL DROP TABLE #MatchedCohort;
+SELECT 
+  c.MatchingPatientId AS FK_Patient_Link_ID,
+  MatchingCovidPositiveDate AS IndexDate,
+  Sex,
+  MatchingYearOfBirth,
+  PatientId AS PatientWhoIsMatched
+INTO #MatchedCohort
+FROM #CohortStore c
+WHERE c.PatientId IN (SELECT FK_Patient_Link_ID FROM #PostCOVIDPatients);
+
+-- Define a table with all the patient ids and index dates for the main cohort and the matched cohort
+IF OBJECT_ID('tempdb..#PatientIdsAndIndexDates') IS NOT NULL DROP TABLE #PatientIdsAndIndexDates;
+SELECT PatientId AS FK_Patient_Link_ID, IndexDate INTO #PatientIdsAndIndexDates FROM #CohortStore
+UNION
+SELECT MatchingPatientId, MatchingCovidPositiveDate FROM #CohortStore;
+
