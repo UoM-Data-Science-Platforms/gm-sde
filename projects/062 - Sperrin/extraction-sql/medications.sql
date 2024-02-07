59a438c7a38cce2328874c66c652906a7bdefa91
George Tilston <george.tilston@manchester.ac.uk>
2024-02-07 12:38:50 +0000

    update extraction files

--- projects/062 - Sperrin/extraction-sql/medications.sql
+++ projects/062 - Sperrin/extraction-sql/medications.sql
@@ -18,7 +18,7 @@ SET NOCOUNT ON;
 
 DECLARE @StartDate datetime;
 DECLARE @EndDate datetime;
-SET @StartDate = '2013-09-01'; --- UPDATE !!!!!!!
+SET @StartDate = '1900-09-01'; --- UPDATE !!!!!!!
 SET @EndDate = '2023-08-31';
 
 --┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -262,7 +262,6 @@ WHERE GPPracticeCode IS NOT NULL AND YearAndQuarterMonthOfBirth < '1963-09-01'
 
 -- load codesets needed for retrieving medication prescriptions
 
-/* antihypertensives */
 -- >>> Codesets required... Inserting the code set code
 --
 --┌────────────────────┐
@@ -705,7 +704,6 @@ sub ON sub.concept = c.concept AND c.version = sub.maxVersion;
 --#endregion
 
 -- >>> Following code sets injected: calcium-channel-blockers v1/beta-blockers v1
-
 -- >>> Following code sets injected: statins v1/ace-inhibitor v1/diuretic v1
 -- >>> Following code sets injected: angiotensin-receptor-blockers v1/acetylcholinesterase-inhibitors v1
 
@@ -737,6 +735,12 @@ WHERE m.FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
 		OR
 		m.FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets_1))
 
+
+--- create index on meds table
+DROP INDEX IF EXISTS medsdata1 ON #medications_rx;
+CREATE INDEX medsdata1 ON #medications_rx (Concept) INCLUDE (FK_Patient_Link_ID, PrescriptionDate);
+
+/*
 -- Dosage information *might* contain sensitive information, so let's 
 -- restrict to dosage instructions that occur >= 50 times
 IF OBJECT_ID('tempdb..#SafeDosages') IS NOT NULL DROP TABLE #SafeDosages;
@@ -744,7 +748,6 @@ SELECT Dosage INTO #SafeDosages FROM #medications_rx
 group by Dosage
 having count(*) >= 50;
 
-/*
 select FK_Patient_Link_ID,
 		PrescriptionDate,
 		Concept,
@@ -754,15 +757,62 @@ from #medications_rx m
 LEFT OUTER JOIN #SafeDosages ON m.Dosage = #SafeDosages.Dosage
 */
 
-select FK_Patient_Link_ID,
-	  Concept,
-		YEAR(PrescriptionDate) AS PrescriptionYear,
-		count(*) as Count
-from #medications_rx m
-LEFT OUTER JOIN #SafeDosages ON m.Dosage = #SafeDosages.Dosage
-group by FK_Patient_Link_ID,
-		Concept,
-		YEAR(PrescriptionDate)
-order by FK_Patient_Link_ID,
-		Concept,
-		YEAR(PrescriptionDate)
+
+---- Find 70th birthday of each patient (to the closest quarter), for working out closest meds before and after 
+IF OBJECT_ID('tempdb..#70thBirthdayDates') IS NOT NULL DROP TABLE #70thBirthdayDates;
+SELECT p.FK_Patient_Link_ID, 
+			Age = YEAR(GETDATE()) - YEAR(yob.YearAndQuarterMonthOfBirth),
+		[70thBirthday] = CONVERT(DATE,CAST(YEAR(YearAndQuarterMonthOfBirth) + 70 AS VARCHAR(4))+'-'+ CAST(MONTH(YearAndQuarterMonthOfBirth) AS VARCHAR(2))+'-'+'01')
+INTO #70thBirthdayDates
+FROM #Patients p
+LEFT OUTER JOIN #PatientYearAndQuarterMonthOfBirth yob ON yob.FK_Patient_Link_ID = p.FK_Patient_Link_ID;
+
+-- Last Prescriptions before 2013-09-01
+IF OBJECT_ID('tempdb..#last_rx_before_2013_09_01') IS NOT NULL DROP TABLE #last_rx_before_2013_09_01;
+SELECT FK_Patient_Link_ID, Concept, MAX(PrescriptionDate) as LastRxBefore2013_09_01
+INTO #last_rx_before_2013_09_01
+FROM #medications_rx m
+WHERE PrescriptionDate < '2013-09-01'
+GROUP BY FK_Patient_Link_ID, Concept
+
+-- First Prescription after 2013-09-01
+IF OBJECT_ID('tempdb..#first_rx_after_2013_09_01') IS NOT NULL DROP TABLE #first_rx_after_2013_09_01;
+SELECT FK_Patient_Link_ID, Concept, MIN(PrescriptionDate) as FirstRxAfter2013_09_01
+INTO #first_rx_after_2013_09_01
+FROM #medications_rx m
+WHERE PrescriptionDate > '2013-09-01'
+GROUP BY FK_Patient_Link_ID, Concept
+
+-- Last Prescription before 70th birthday
+IF OBJECT_ID('tempdb..#last_rx_before_70th') IS NOT NULL DROP TABLE #last_rx_before_70th;
+SELECT m.FK_Patient_Link_ID, Concept, MAX(PrescriptionDate) as LastRxBefore70thBirthday
+INTO #last_rx_before_70th
+FROM #medications_rx m
+LEFT JOIN #70thBirthdayDates bd ON bd.FK_Patient_Link_ID = m.FK_Patient_Link_ID
+WHERE PrescriptionDate < [70thBirthday]
+GROUP BY m.FK_Patient_Link_ID, Concept
+
+-- First Prescription after 70th birthday
+IF OBJECT_ID('tempdb..#first_rx_after_70th') IS NOT NULL DROP TABLE #first_rx_after_70th;
+SELECT m.FK_Patient_Link_ID, Concept, MIN(PrescriptionDate) as FirstRxAfter70thBirthday
+INTO #first_rx_after_70th
+FROM #medications_rx m
+LEFT JOIN #70thBirthdayDates bd ON bd.FK_Patient_Link_ID = m.FK_Patient_Link_ID
+WHERE PrescriptionDate > [70thBirthday]
+GROUP BY m.FK_Patient_Link_ID, Concept
+
+-- bring together for final output at patient level
+
+SELECT PatientId = m.FK_Patient_Link_ID,
+	m.Concept,
+	FirstRx = MIN(PrescriptionDate), 
+	LastRxBefore2013_09_01= MAX(LastRxBefore2013_09_01),
+	FirstRxAfter2013_09_01 = MIN(FirstRxAfter2013_09_01),
+	LastRxBefore70thBirthday = MAX(LastRxBefore70thBirthday), --inform PI that this is only accurate to the nearest quarter
+	FirstRxAfter70thBirthday = MIN(FirstRxAfter70thBirthday) -- --inform PI that this is only accurate to the nearest quarter
+FROM #medications_rx m
+LEFT JOIN #last_rx_before_2013_09_01 t1 ON t1.FK_Patient_Link_ID = m.FK_Patient_Link_ID AND t1.Concept = m.Concept
+LEFT JOIN #first_rx_after_2013_09_01 t2 ON t2.FK_Patient_Link_ID = m.FK_Patient_Link_ID AND t2.Concept = m.Concept
+LEFT JOIN #last_rx_before_70th t3 ON t3.FK_Patient_Link_ID = m.FK_Patient_Link_ID AND t3.Concept = m.Concept
+LEFT JOIN #first_rx_after_70th t4 ON t4.FK_Patient_Link_ID = m.FK_Patient_Link_ID AND t4.Concept = m.Concept
+GROUP BY m.FK_Patient_Link_ID, m.Concept
