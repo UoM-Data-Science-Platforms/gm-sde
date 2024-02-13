e2f27deb6d07545abdca2ecfc35852b02da61309
George Tilston <george.tilston@manchester.ac.uk>
2024-02-13 16:23:36 +0000

    update hosp admission template to use reusable query

--- projects/062 - Sperrin/template-sql/hospital-admission.template.sql
+++ projects/062 - Sperrin/template-sql/hospital-admission.template.sql
@@ -22,43 +22,7 @@ SET @StartDate = '2014-01-01';
 
 
 --> EXECUTE query-build-rq062-cohort.sql
-
-
--- Populate temporary table with admissions
--- Convert AdmissionDate to a date to avoid issues where a person has two admissions
--- on the same day (but only one discharge)
-IF OBJECT_ID('tempdb..#Admissions') IS NOT NULL DROP TABLE #Admissions;
-CREATE TABLE #Admissions (
-	FK_Patient_Link_ID BIGINT,
-	AdmissionDate DATE,
-	AcuteProvider NVARCHAR(150)
-);
-
-INSERT INTO #Admissions
-SELECT DISTINCT FK_Patient_Link_ID, CONVERT(DATE, AdmissionDate) AS AdmissionDate, t.TenancyName AS AcuteProvider
-FROM [SharedCare].[Acute_Inpatients] i
-LEFT OUTER JOIN SharedCare.Reference_Tenancy t ON t.PK_Reference_Tenancy_ID = i.FK_Reference_Tenancy_ID
-WHERE EventType = 'Admission'
-AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
-AND AdmissionDate >= @StartDate;
-
-
---> EXECUTE query-get-discharges.sql all-patients:false
-
--- Link admission with discharge to get length of stay
--- Length of stay is zero-indexed e.g. 
--- 1 = [0,1) days
--- 2 = [1,2) days
-IF OBJECT_ID('tempdb..#LengthOfStay') IS NOT NULL DROP TABLE #LengthOfStay;
-SELECT 
-	a.FK_Patient_Link_ID, a.AdmissionDate, a.AcuteProvider, 
-	MIN(d.DischargeDate) AS DischargeDate, 
-	1 + DATEDIFF(day,a.AdmissionDate, MIN(d.DischargeDate)) AS LengthOfStay
-INTO #LengthOfStay
-FROM #Admissions a
-INNER JOIN #Discharges d ON d.FK_Patient_Link_ID = a.FK_Patient_Link_ID AND d.DischargeDate >= a.AdmissionDate AND d.AcuteProvider = a.AcuteProvider
-GROUP BY a.FK_Patient_Link_ID, a.AdmissionDate, a.AcuteProvider
-ORDER BY a.FK_Patient_Link_ID, a.AdmissionDate, a.AcuteProvider;
+--> EXECUTE query-get-admissions-and-length-of-stay.sql all-patients:false
 
 ----- create anonymised identifier for each hospital
 -- this is included in case PI wants to consider the fact that each hospitalstarted providing complete data on different dates
@@ -79,8 +43,8 @@ SELECT FK_Patient_Link_ID AS PatientID,
  	   YearAndMonthOfAdmission = DATEADD(dd, -( DAY( AdmissionDate) -1 ), AdmissionDate),
 	   LengthOfStayDays = LengthOfStay,
 	   HospitalID 
-FROM #LengthOfStay los
-LEFT JOIN #RandomiseHospital rh ON rh.AcuteProvider = los.AcuteProvider
+FROM #Admissions a
+LEFT JOIN #RandomiseHospital rh ON rh.AcuteProvider = a.AcuteProvider
 ORDER BY FK_Patient_Link_ID, AdmissionDate
 
 
