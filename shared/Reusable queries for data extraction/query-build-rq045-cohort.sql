af58efd7a91ee85f7d989ef03ffd62b5b999acac
George Tilston <george.tilston@manchester.ac.uk>
2024-02-23 10:09:09 +0000

    cohort script

--- shared/Reusable queries for data extraction/query-build-rq045-cohort.sql
+++ shared/Reusable queries for data extraction/query-build-rq045-cohort.sql
@@ -14,40 +14,35 @@
 -- - LSOA
 -- - Ethnicity
 
-
--- Create the #Patients table================================================================================================================================
-IF OBJECT_ID('tempdb..#Patients') IS NOT NULL DROP TABLE #Patients;
-SELECT PK_Patient_Link_ID AS FK_Patient_Link_ID INTO #Patients
-FROM SharedCare.Patient_Link
-
+DECLARE @StartDate datetime;
+SET @StartDate = '2020-01-01';
 
 --=========================================================================================================================================================== 
+--> EXECUTE query-get-possible-patients.sql
 --> EXECUTE query-patient-sex.sql
 --> EXECUTE query-patient-year-of-birth.sql
 --> EXECUTE query-patient-lsoa.sql
 
 
--- Create the table of ethnic================================================================================================================================
-IF OBJECT_ID('tempdb..#Ethnic') IS NOT NULL DROP TABLE #Ethnic;
-SELECT PK_Patient_Link_ID AS FK_Patient_Link_ID, EthnicCategoryDescription AS Ethnicity
-INTO #Ethnic
-FROM SharedCare.Patient_Link;
-
-
 -- The cohort table========================================================================================================================================
 IF OBJECT_ID('tempdb..#Cohort') IS NOT NULL DROP TABLE #Cohort;
 SELECT
-  p.FK_Patient_Link_ID
+  p.FK_Patient_Link_ID,
+  Sex,
+  YearOfBirth,
+  LSOA_Code,
+  Ethnicity = EthnicGroupDescription,
+  DeathDate
 INTO #Cohort
 FROM #Patients p
-LEFT OUTER JOIN #Ethnic e ON e.FK_Patient_Link_ID = p.FK_Patient_Link_ID
 LEFT OUTER JOIN #PatientYearOfBirth y ON p.FK_Patient_Link_ID = y.FK_Patient_Link_ID
 LEFT OUTER JOIN #PatientSex sex ON sex.FK_Patient_Link_ID = p.FK_Patient_Link_ID
 LEFT OUTER JOIN #PatientLSOA l ON l.FK_Patient_Link_ID = p.FK_Patient_Link_ID
-WHERE e.Ethnicity IS NOT NULL AND y.YearOfBirth IS NOT NULL AND sex.Sex IS NOT NULL AND l.LSOA_Code IS NOT NULL;
+WHERE y.YearOfBirth IS NOT NULL AND sex.Sex IS NOT NULL AND l.LSOA_Code IS NOT NULL
+	AND YEAR(GETDATE()) - y.YearOfBirth >= 18;
 
 
 -- Change the cohort table name into #Patients to use for other reusable queries===========================================================================
-TRUNCATE TABLE #Patients;
-INSERT INTO #Patients
-SELECT FK_Patient_Link_ID FROM #Cohort;
+DELETE FROM #Patients
+WHERE FK_Patient_Link_ID NOT IN 
+	(SELECT FK_Patient_Link_ID FROM #Cohort);
