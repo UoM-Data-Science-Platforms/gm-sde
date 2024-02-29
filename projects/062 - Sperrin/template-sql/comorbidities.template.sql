1603976a581e6e109db3db5ef0ba10a9f1042f37
George Tilston <george.tilston@manchester.ac.uk>
2024-02-29 09:55:16 +0000

    update comorbidity file RQ062
    - add code concept to final file
    - incorporate med codes into shingles vaccination
    - remove EventCodeSystem field as not needed

--- projects/062 - Sperrin/template-sql/comorbidities.template.sql
+++ projects/062 - Sperrin/template-sql/comorbidities.template.sql
@@ -29,6 +29,11 @@ INTO #GPEvents
 FROM SharedCare.GP_Events
 WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients);
 
+--- create index on GP events table
+DROP INDEX IF EXISTS GPEventIndex ON #GPEvents;
+CREATE INDEX GPEventIndex ON #GPEvents (SuppliedCode) INCLUDE (FK_Patient_Link_ID, EventDate);
+
+
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:Shingles condition:shingles
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:PostherpeticNeuralgia condition:post-herpetic-neuralgia
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:CHD condition:coronary-heart-disease
@@ -47,50 +52,101 @@ WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients);
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:BreastCancerScreening condition:breast-cancer-screening
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:ColorectalCancerScreening condition:colorectal-cancer-screening
 --> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:RTI condition:respiratory-tract-infection
+--> EXECUTE query-build-rq062-gp-events.sql version:1 conditionname:ShinglesVaccine condition:shingles-vaccination
 
+
+--> CODESET flu-vaccine:1
+
+DROP TABLE #GPEvents
+
+-- get all vaccination records from GP_Medications table to add to the event codes later on
+
+-- first for flu vaccination meds
+IF OBJECT_ID('tempdb..#FluVaccineMed') IS NOT NULL DROP TABLE #FluVaccineMed;
+CREATE TABLE #FluVaccineMed (PatientId BIGINT NOT NULL, EventDate DATE, 
+EventCode VARCHAR(255), EventDescription VARCHAR(255) COLLATE Latin1_General_CS_AS /*, EventCodeSystem VARCHAR(255)*/);
+
+INSERT INTO #FluVaccineMed (PatientId, EventDate, EventCode, EventDescription)
+SELECT FK_Patient_Link_ID, 
+		CAST(MedicationDate AS DATE), 
+		SuppliedCode,
+		a.description
+FROM SharedCare.GP_Medications gp
+LEFT OUTER JOIN #AllCodes a ON gp.SuppliedCode = a.Code
+WHERE SuppliedCode IN 
+	(SELECT Code FROM #AllCodes WHERE (Concept = 'flu-vaccine' AND [Version] = 1)) 
+AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
+
+-- second for shingles vaccination meds
+IF OBJECT_ID('tempdb..#ShinglesVaccineMed') IS NOT NULL DROP TABLE #ShinglesVaccineMed;
+CREATE TABLE #ShinglesVaccineMed (PatientId BIGINT NOT NULL, EventDate DATE, 
+EventCode VARCHAR(255), EventDescription VARCHAR(255) COLLATE Latin1_General_CS_AS /*, EventCodeSystem VARCHAR(255)*/);
+
+INSERT INTO #ShinglesVaccineMed (PatientId, EventDate, EventCode, EventDescription)
+SELECT FK_Patient_Link_ID, 
+		CAST(MedicationDate AS DATE), 
+		SuppliedCode,
+		a.description
+FROM SharedCare.GP_Medications gp
+LEFT OUTER JOIN #AllCodes a ON gp.SuppliedCode = a.Code
+WHERE SuppliedCode IN 
+	(SELECT Code FROM #AllCodes WHERE (Concept = 'shingles-vaccination' AND [Version] = 1)) 
+AND FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
 
 -- Create the final table==========================================================================================================
 IF OBJECT_ID('tempdb..#Final') IS NOT NULL DROP TABLE #Final;
-SELECT * INTO #Final FROM #BackProblems
+SELECT *, 'back-problems' as Condition INTO #Final FROM #BackProblems
 UNION
-SELECT * FROM #BreastCancer
+SELECT *, 'breast-cancer' as Condition FROM #BreastCancer
 UNION
-SELECT * FROM #BreastCancerScreening
+SELECT *, 'breast-cancer-screening' as Condition FROM #BreastCancerScreening
 UNION
-SELECT * FROM #CHD
+SELECT *, 'coronary-heart-disease' as Condition FROM #CHD
 UNION
-SELECT * FROM #ColorectalCancer
+SELECT *, 'colorectal-cancer' as Condition FROM #ColorectalCancer
 UNION 
-SELECT * FROM #ColorectalCancerScreening
+SELECT *, 'colorectal-cancer-screening' as Condition FROM #ColorectalCancerScreening
 UNION
-SELECT * FROM #COPD
+SELECT *, 'copd' as Condition FROM #COPD
 UNION 
-SELECT * FROM #Dementia
+SELECT *, 'dementia' as Condition FROM #Dementia
 UNION
-SELECT * FROM #Diabetes
+SELECT *, 'diabetes' as Condition FROM #Diabetes
 UNION
-SELECT * FROM #Falls
+SELECT *, 'falls' as Condition FROM #Falls
 UNION
-SELECT * FROM #FluVaccine
+SELECT *, 'flu-vaccine' as Condition FROM #FluVaccine
 UNION
-SELECT * FROM #LungCancer
+SELECT *, 'lung-cancer' as Condition FROM #LungCancer
 UNION
-SELECT * FROM #PancreaticCancer
+SELECT *, 'pancreatic-cancer' as Condition FROM #PancreaticCancer
 UNION
-SELECT * FROM #PneumococcalVaccine
+SELECT *, 'pneumococcal-vaccine' as Condition FROM #PneumococcalVaccine
 UNION
-SELECT * FROM #PostherpeticNeuralgia
+SELECT *, 'postherpetic-neuralgia' as Condition  FROM #PostherpeticNeuralgia
 UNION
-SELECT * FROM #RTI
+SELECT *, 'respiratory-tract-infection' as Condition  FROM #RTI
 UNION
-SELECT * FROM #Shingles
+SELECT *, 'shingles' as Condition  FROM #Shingles
 UNION
-SELECT * FROM #Stroke
+SELECT *, 'stroke' as Condition  FROM #Stroke
+UNION
+SELECT *, 'shingles-vaccination' as Condition  FROM #ShinglesVaccine
+UNION 
+SELECT *, 'flu-vaccine' as Condition FROM #FluVaccineMed
+UNION 
+SELECT *, 'shingles-vaccination' as Condition FROM #ShinglesVaccineMed
+
+DROP TABLE #Shingles, #PostherpeticNeuralgia, #CHD, #Stroke, #Dementia, #COPD, #LungCancer, 
+#PancreaticCancer, #ColorectalCancer, #BreastCancer, #Falls, #BackProblems, #Diabetes, #FluVaccine, 
+#PneumococcalVaccine, #BreastCancerScreening, #ColorectalCancerScreening, #RTI, #ShinglesVaccine,
+#FluVaccineMed 
 
 SELECT PatientId,
-	EventYearAndMonth = DATEADD(dd, -( DAY(EventDate) -1 ), EventDate), -- hide the day of the admission by setting to first of the month,
+	EventYearAndMonth = DATEADD(dd, -( DAY(EventDate) -1 ), EventDate), -- hide the day of the event by setting to first of the month,
 	EventCode,
-	EventDescription,
-	EventCodeSystem 
+	EventCategory = Condition,
+	EventDescription
+	--EventCodeSystem 
 FROM #Final
 ORDER BY PatientID, EventDate;
