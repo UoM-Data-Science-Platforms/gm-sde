7ca390ef629d7902978e859d982e7639d5a0ca14
George Tilston <george.tilston@manchester.ac.uk>
2024-03-26 14:28:02 +0000

    update DOB reusable queries
    - Simplified to one query for dob which has an output table containing day, week, month, year, and full DOB

--- shared/Reusable queries for data extraction/query-patient-date-of-birth.sql
+++ shared/Reusable queries for data extraction/query-patient-date-of-birth.sql
@@ -1,8 +1,8 @@
---┌────────────────────────────────┐
---│ Year, month and week of birth  │
---└────────────────────────────────┘
+--┌──────────────────────────────────────┐
+--│ Year, month, week, and day of birth  │
+--└──────────────────────────────────────┘
 
--- OBJECTIVE: To get the week of birth for each patient.
+-- OBJECTIVE: To get the date of birth for each patient, in various formats.
 
 -- INPUT: Assumes there exists a temp table as follows:
 -- #Patients (FK_Patient_Link_ID)
@@ -11,16 +11,18 @@
 -- OUTPUT: A temp table as follows:
 -- #PatientWeekOfBirth (FK_Patient_Link_ID, WeekOfBirth)
 -- 	- FK_Patient_Link_ID - unique patient id
---	- WeekOfBirth (Number of the week within a year, e.g. from 1 to 52)
---  - MonthOfBirth (mm)
+--  - DateOfBirthPID (yyyy-mm-dd)  **not to be provided to study teams
+--  - DayOfBirth (dd) (1 to 31)
+--	- WeekOfBirth (ww) (1 to 52)
+--  - MonthOfBirth (mm) (1 to 12)
 --  - YearOfBirth (yyyy)
 
 -- ASSUMPTIONS:
---	- Patient data is obtained from multiple sources. Where patients have multiple WeekOfBirths we determine the WeekOfBirth as follows:
---	-	If the patients has a WeekOfBirth in their primary care data feed we use that as most likely to be up to date
---	-	If every WeekOfBirth for a patient is the same, then we use that
---	-	If there is a single most recently updated WeekOfBirth in the database then we use that
---	-	Otherwise we take the highest WeekOfBirth for the patient that is not in the future
+--	- Patient data is obtained from multiple sources. Where patients have multiple DateOfBirths we determine the DateOfBirth as follows:
+--	-	If the patients has a DateOfBirth in their primary care data feed we use that as most likely to be up to date
+--	-	If every DateOfBirth for a patient is the same, then we use that
+--	-	If there is a single most recently updated DateOfBirth in the database then we use that
+--	-	Otherwise we take the highest DateOfBirth for the patient that is not in the future
 
 -- Get all patients year and quarter month of birth for the cohort
 IF OBJECT_ID('tempdb..#AllPatientDateOfBirths') IS NOT NULL DROP TABLE #AllPatientDateOfBirths;
@@ -37,8 +39,8 @@ AND Dob IS NOT NULL;
 
 -- If patients have a tenancy id of 2 we take this as their most likely DateOfBirth
 -- as this is the GP data feed and so most likely to be up to date
-IF OBJECT_ID('tempdb..#PatientDateOfBirth') IS NOT NULL DROP TABLE #PatientDateOfBirth;
-SELECT FK_Patient_Link_ID, MIN(DateOfBirth) as DateOfBirthPID INTO #PatientDateOfBirth FROM #AllPatientDateOfBirths
+IF OBJECT_ID('tempdb..#PatientDateOfBirthTEMP') IS NOT NULL DROP TABLE #PatientDateOfBirthTEMP;
+SELECT FK_Patient_Link_ID, MIN(DateOfBirth) as DateOfBirthPID INTO #PatientDateOfBirthTEMP FROM #AllPatientDateOfBirths
 WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Patients)
 AND FK_Reference_Tenancy_ID = 2
 GROUP BY FK_Patient_Link_ID
@@ -48,10 +50,10 @@ HAVING MIN(DateOfBirth) = MAX(DateOfBirth);
 IF OBJECT_ID('tempdb..#UnmatchedYobPatients') IS NOT NULL DROP TABLE #UnmatchedYobPatients;
 SELECT FK_Patient_Link_ID INTO #UnmatchedYobPatients FROM #Patients
 EXCEPT
-SELECT FK_Patient_Link_ID FROM #PatientDateOfBirth;
+SELECT FK_Patient_Link_ID FROM #PatientDateOfBirthTEMP;
 
 -- If every DateOfBirth is the same for all their linked patient ids then we use that
-INSERT INTO #PatientDateOfBirth
+INSERT INTO #PatientDateOfBirthTEMP
 SELECT FK_Patient_Link_ID, MIN(DateOfBirth) FROM #AllPatientDateOfBirths
 WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #UnmatchedYobPatients)
 GROUP BY FK_Patient_Link_ID
@@ -62,10 +64,10 @@ TRUNCATE TABLE #UnmatchedYobPatients;
 INSERT INTO #UnmatchedYobPatients
 SELECT FK_Patient_Link_ID FROM #Patients
 EXCEPT
-SELECT FK_Patient_Link_ID FROM #PatientDateOfBirth;
+SELECT FK_Patient_Link_ID FROM #PatientDateOfBirthTEMP;
 
 -- If there is a unique most recent DateOfBirth then use that
-INSERT INTO #PatientDateOfBirth
+INSERT INTO #PatientDateOfBirthTEMP
 SELECT p.FK_Patient_Link_ID, MIN(p.DateOfBirth) FROM #AllPatientDateOfBirths p
 INNER JOIN (
 	SELECT FK_Patient_Link_ID, MAX(HDMModifDate) MostRecentDate FROM #AllPatientDateOfBirths
@@ -80,10 +82,10 @@ TRUNCATE TABLE #UnmatchedYobPatients;
 INSERT INTO #UnmatchedYobPatients
 SELECT FK_Patient_Link_ID FROM #Patients
 EXCEPT
-SELECT FK_Patient_Link_ID FROM #PatientDateOfBirth;
+SELECT FK_Patient_Link_ID FROM #PatientDateOfBirthTEMP;
 
 -- Otherwise just use the highest value (with the exception that can't be in the future)
-INSERT INTO #PatientDateOfBirth
+INSERT INTO #PatientDateOfBirthTEMP
 SELECT FK_Patient_Link_ID, MAX(DateOfBirth) FROM #AllPatientDateOfBirths
 WHERE FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #UnmatchedYobPatients)
 GROUP BY FK_Patient_Link_ID
@@ -93,13 +95,14 @@ HAVING MAX(DateOfBirth) <= GETDATE();
 DROP TABLE #AllPatientDateOfBirths;
 DROP TABLE #UnmatchedYobPatients;
 
--- Mask 'day' in date of birth by converting to week of birth
+-- Final table providing date of birth at different levels of granularity
 
-IF OBJECT_ID('tempdb..#PatientWeekOfBirth') IS NOT NULL DROP TABLE #PatientWeekOfBirth;
+IF OBJECT_ID('tempdb..#PatientDateOfBirth') IS NOT NULL DROP TABLE #PatientDateOfBirth;
 SELECT FK_Patient_Link_ID,
 	DateOfBirthPID, -- this is included in this table incase it is needed (e.g. to calculate an accurate age)
+	DayOfBirth = DATEPART(Day,DateOfBirthPID),
 	WeekOfBirth = DATEPART(Week,DateOfBirthPID),
     MonthOfBirth = DATEPART(Month,DateOfBirthPID),
     YearOfBirth = DATEPART(Year,DateOfBirthPID) 
-INTO #PatientWeekOfBirth
-FROM #PatientDateOfBirth
+INTO #PatientDateOfBirth
+FROM #PatientDateOfBirthTEMP
