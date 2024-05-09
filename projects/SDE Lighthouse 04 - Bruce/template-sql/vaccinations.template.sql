9136b4371a2ea2164b4ba604fbd8813622392f2b
George Tilston <george.tilston@manchester.ac.uk>
2024-05-09 14:04:42 +0100

    updates to LH004 draft scripts

---
+++ projects/SDE Lighthouse 04 - Bruce/template-sql/vaccinations.template.sql
@@ -0,0 +1,42 @@
+--┌────────────────────────────────────┐
+--│ LH004 Vaccinations file            │
+--└────────────────────────────────────┘
+
+-- OUTPUT: Data with the following fields
+-- 	- PatientId (int)
+--  - Date
+--  - VaccinationType
+
+--Just want the output, not the messages
+SET NOCOUNT ON;
+
+DECLARE @StartDate datetime;
+DECLARE @EndDate datetime;
+SET @StartDate = '2020-01-01';  -- CHECK
+SET @EndDate = '2023-10-31'; -- CHECK
+
+--> EXECUTE query-build-lh004-cohort.sql
+
+--> CODESET flu-vaccination:1 covid-vaccination:1 pneumococcal-vaccination:1 shingles-vaccination:1
+
+-- TABLE OF ALL INFECTIONS FOR THE COHORT
+
+IF OBJECT_ID('tempdb..#Infections') IS NOT NULL DROP TABLE #Infections;
+SELECT FK_Patient_Link_ID,
+	EventDate = CAST(EventDate as DATE)
+INTO #Infections
+FROM SharedCare.GP_Events m
+LEFT OUTER JOIN #VersionedSnomedSets s ON s.FK_Reference_SnomedCT_ID = m.FK_Reference_SnomedCT_ID
+LEFT OUTER JOIN #VersionedCodeSets c ON c.FK_Reference_Coding_ID = m.FK_Reference_Coding_ID
+WHERE m.FK_Patient_Link_ID IN (SELECT FK_Patient_Link_ID FROM #Cohort)
+AND m.EventDate BETWEEN @StartDate AND @EndDate
+AND (
+	m.FK_Reference_SnomedCT_ID IN (SELECT FK_Reference_SnomedCT_ID FROM #VersionedSnomedSets WHERE Concept = 'infections') OR
+    m.FK_Reference_Coding_ID IN (SELECT FK_Reference_Coding_ID FROM #VersionedCodeSets WHERE Concept = 'infections')
+);
+
+
+--bring together for final output
+SELECT	 PatientId = m.FK_Patient_Link_ID,
+		EventDate
+FROM #Infections m
