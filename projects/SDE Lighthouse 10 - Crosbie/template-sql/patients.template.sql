93ebc8143e2cd0cbf945e5021e6cfbc2b0a3545e
George Tilston <george.tilston@manchester.ac.uk>
2024-07-15 10:08:08 +0100

    Set up folder for lh010

---
+++ projects/SDE Lighthouse 10 - Crosbie/template-sql/patients.template.sql
@@ -0,0 +1,33 @@
+--┌────────────────────────────────────┐
+--│ SDE Lighthouse study 10 - Crosbie  │
+--└────────────────────────────────────┘
+
+--Just want the output, not the messages
+SET NOCOUNT ON;
+
+DECLARE @StartDate datetime;
+DECLARE @EndDate datetime;
+SET @StartDate = 'CHANGE'; -- CHECK THIS AND  - CURRENTLY EXCLUDING ANY PATIENTS THAT WEREN'T 18 IN 2006
+SET @EndDate = 'CHANGE';
+
+--> EXECUTE query-build-lh009-cohort.sql
+
+--> EXECUTE query-patient-lsoa.sql
+--> EXECUTE query-patient-imd.sql
+--> EXECUTE query-patient-smoking-status.sql
+--> EXECUTE query-patient-alcohol-intake.sql
+
+--bring together for final output
+--patients in main cohort
+SELECT	 PatientId = FK_Patient_Link_ID
+		,YearOfBirth
+		,Sex
+		,LSOA_Code
+		,EthnicMainGroup ----- CHANGE TO MORE SPECIFIC ETHNICITY ?
+		,IMD2019Decile1IsMostDeprived10IsLeastDeprived
+		,DeathDate
+FROM #Cohort m
+LEFT OUTER JOIN #PatientLSOA lsoa ON lsoa.FK_Patient_Link_ID = m.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientIMDDecile imd ON imd.FK_Patient_Link_ID = m.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientSmokingStatus smok ON smok.FK_Patient_Link_ID = m.FK_Patient_Link_ID
+LEFT OUTER JOIN #PatientAlcoholIntake alc ON alc.FK_Patient_Link_ID = m.FK_Patient_Link_ID
