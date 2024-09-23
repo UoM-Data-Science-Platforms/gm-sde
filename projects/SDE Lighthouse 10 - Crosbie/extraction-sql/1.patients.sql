444cb4af0f877dee8bb3c738f86d6799833a5bd7
George Tilston <george.tilston@manchester.ac.uk>
2024-09-23 15:32:38 +0100

    updates to LH010 draft scripts

--- projects/SDE Lighthouse 10 - Crosbie/extraction-sql/1.patients.sql
+++ projects/SDE Lighthouse 10 - Crosbie/extraction-sql/1.patients.sql
@@ -1,3 +1,5 @@
+USE SCHEMA SDE_REPOSITORY.SHARED_UTILITIES;
+
 --┌────────────────────────────────────┐
 --│ LH010 Patient file                 │
 --└────────────────────────────────────┘
@@ -62,8 +64,8 @@ WHERE
 
 -- GET COHORT OF PATIENTS THAT HAD A LUNG HEALTH CHECK
 
-DROP TABLE IF EXISTS {{cohort-table}};
-CREATE TABLE {{cohort-table}} AS 
+DROP TABLE IF EXISTS SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie";
+CREATE TABLE SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie" AS 
 SELECT "GmPseudo", "FK_Patient_ID" 
 FROM LatestSnapshotAdults
 --LEFT OUTER JOIN **LUNGHEALTHCHECKTABLE**
@@ -78,11 +80,13 @@ CREATE TEMPORARY TABLE PersonalHistoryCancer AS
 SELECT DISTINCT ltc."GmPseudo", "FK_Patient_ID"
 FROM PRESENTATION.GP_RECORD."LongTermConditionRegister_SecondaryUses" ltc
 WHERE ("Cancer_QOF" is not null or "Cancer_DiagnosisDate" is not null or "Cancer_DiagnosisAge" is not null or "Cancer_QOF_DiagnosedL5Y" is not null)
-	AND "GmPseudo" IN (SELECT "GmPseudo" FROM {{cohort-table}});
+	AND "GmPseudo" IN (SELECT "GmPseudo" FROM SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie");
 
 -- BMI is available in demographics table, but for height and weight we need to use GMCR code sets
 
---> CODESET height:1 weight:1
+-- >>> Codesets required... Inserting the code set code
+-- >>> Codesets extracted into 0.code-sets.sql
+-- >>> Following code sets injected: height v1/weight v1
 
 DROP TABLE IF EXISTS HeightWeight ;
 CREATE TEMPORARY TABLE HeightWeight AS
@@ -95,10 +99,10 @@ SELECT DISTINCT
 	, cs.concept
 	, e."Term" AS "Description"
 FROM INTERMEDIATE.GP_RECORD."GP_Events_SecondaryUses" e
-LEFT OUTER JOIN {{code-set-table}} cs ON cs.code = e."SuppliedCode"
+LEFT OUTER JOIN SDE_REPOSITORY.SHARED_UTILITIES."Code_Sets_SDE_Lighthouse_10_Crosbie" cs ON cs.code = e."SuppliedCode"
 LEFT OUTER JOIN PRESENTATION.GP_RECORD."DemographicsProtectedCharacteristics_SecondaryUses" dem ON dem."FK_Patient_ID" = e."FK_Patient_ID" -- join to demographics table to get GmPseudo
 WHERE cs.concept IN ('height', 'weight')
-	AND e."FK_Patient_ID" IN (SELECT "FK_Patient_ID" FROM {{cohort-table}})
+	AND e."FK_Patient_ID" IN (SELECT "FK_Patient_ID" FROM SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie")
 	AND "EventDate" <= '2016-01-01';
 
 -- FIND CLOSEST HEIGHT AND WEIGHT INFO BEFORE 2016 (START DATE)
@@ -130,7 +134,7 @@ CREATE TEMPORARY TABLE Statins AS
 SELECT	c."GmPseudo"
     , MIN(TO_DATE(ec."MedicationDate")) AS "MinStatinDate"
 FROM INTERMEDIATE.GP_RECORD."MedicationsClusters" ec
-INNER JOIN {{cohort-table}} c ON c."FK_Patient_ID" = ec."FK_Patient_ID"
+INNER JOIN SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie" c ON c."FK_Patient_ID" = ec."FK_Patient_ID"
 WHERE ec."Field_ID" = 'Statin'
 	AND ec."MedicationDate" < '2016-01-01'
 GROUP BY c."GmPseudo";
@@ -141,7 +145,7 @@ CREATE TEMPORARY TABLE COPDMeds AS
 SELECT	c."GmPseudo"
     , MIN(TO_DATE(ec."MedicationDate")) AS "MinCOPDMedDate"
 FROM INTERMEDIATE.GP_RECORD."MedicationsClusters" ec
-INNER JOIN {{cohort-table}} c ON c."FK_Patient_ID" = ec."FK_Patient_ID"
+INNER JOIN SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie" c ON c."FK_Patient_ID" = ec."FK_Patient_ID"
 WHERE ec."Field_ID" = 'COPDICSDRUG_COD'
 	AND ec."MedicationDate" < '2016-01-01'
 GROUP BY c."GmPseudo";
@@ -150,7 +154,15 @@ GROUP BY c."GmPseudo";
 
 -- FOR THE ABOVE COHORT, GET ALL REQUIRED DEMOGRAPHICS
 
-{{create-output-table::"1_Patients"}}
+
+-- ... processing [[create-output-table::"1_Patients"]] ... 
+-- ... Need to create an output table called "1_Patients" and replace 
+-- ... the GmPseudo column with a study-specific random patient id.
+
+-- First we create a table in an area only visible to the RDEs which contains
+-- the GmPseudos. THESE CANNOT BE RELEASED TO END USERS.
+DROP TABLE IF EXISTS SDE_REPOSITORY.SHARED_UTILITIES."1_Patients_WITH_PSEUDO_IDS";
+CREATE TABLE SDE_REPOSITORY.SHARED_UTILITIES."1_Patients_WITH_PSEUDO_IDS" AS
 SELECT
 	 dem."GmPseudo", 
 	 dem."Sex",
@@ -182,7 +194,7 @@ SELECT
 	 st."MinStatinDate" AS "EarliestStatinPrescription_Date",
 	 CASE WHEN copd."GmPseudo" IS NOT NULL THEN 1 ELSE 0 END AS "COPDMedPrescribedBeforeJan2016",
 	 copd."MinCOPDMedDate" AS "EarliestCOPDMedPrescription_Date"
-FROM {{cohort-table}}  co
+FROM SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie"  co
 LEFT OUTER JOIN PRESENTATION.GP_RECORD."DemographicsProtectedCharacteristics_SecondaryUses" dem ON dem."GmPseudo" = co."GmPseudo"
 LEFT OUTER JOIN Death dth ON dth."GmPseudo" = co."GmPseudo"
 LEFT OUTER JOIN PersonalHistoryCancer phc ON phc."GmPseudo" = co."GmPseudo"
@@ -190,3 +202,35 @@ LEFT OUTER JOIN HeightWeightFinal hwf ON hwf."GmPseudo" = co."GmPseudo"
 LEFT OUTER JOIN Statins st ON st."GmPseudo" = co."GmPseudo"
 LEFT OUTER JOIN COPDMeds copd ON copd."GmPseudo" = co."GmPseudo"
 QUALIFY row_number() OVER (PARTITION BY dem."GmPseudo" ORDER BY "Snapshot" DESC) = 1;
+
+-- Then we check to see if there are any new GmPseudo ids. We do this by making a temp table 
+-- of all "new" GmPseudo ids. I.e. any GmPseudo ids that we've already got a unique id for
+-- for this study are excluded
+DROP TABLE IF EXISTS "AllPseudos_SDE_Lighthouse_10_Crosbie";
+CREATE TEMPORARY TABLE "AllPseudos_SDE_Lighthouse_10_Crosbie" AS
+SELECT DISTINCT "GmPseudo" FROM SDE_REPOSITORY.SHARED_UTILITIES."1_Patients_WITH_PSEUDO_IDS"
+EXCEPT
+SELECT "GmPseudo" FROM "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie";
+
+-- Find the highest currently assigned id. Ids are given incrementally, so now ones
+-- need to start at +1 of the current highest
+SET highestPatientId = (
+    SELECT IFNULL(MAX("StudyPatientPseudoId"),0) FROM "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie"
+);
+
+-- Make a study specific hash for each new GmPseudo and insert it
+-- into the patient lookup table
+INSERT INTO "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie"
+SELECT
+    "GmPseudo", -- the GM SDE patient ids for patients in this cohort
+    SHA2(CONCAT('SDE_Lighthouse_10_Crosbie', "GmPseudo")) AS "Hash", -- used to provide a random (study-specific) ordering for the patient ids we provide
+    $highestPatientId + ROW_NUMBER() OVER (ORDER BY "Hash") -- the patient id that we provide to the analysts
+FROM "AllPseudos_SDE_Lighthouse_10_Crosbie";
+
+-- Finally, we select from the output table which includes the GmPseudos, in order
+-- to populate the table for the end users where the GmPseudo fields are redacted via a function
+-- created in the 0.code-sets.sql file
+DROP TABLE IF EXISTS SDE_REPOSITORY.SHARED_UTILITIES."1_Patients";
+CREATE TABLE SDE_REPOSITORY.SHARED_UTILITIES."1_Patients" AS
+SELECT SDE_REPOSITORY.SHARED_UTILITIES.gm_pseudo_hash_SDE_Lighthouse_10_Crosbie("GmPseudo") AS "PatientID", * EXCLUDE "GmPseudo"
+FROM SDE_REPOSITORY.SHARED_UTILITIES."1_Patients_WITH_PSEUDO_IDS";
--- projects/SDE Lighthouse 10 - Crosbie/extraction-sql/2.comorbidities.sql
+++ projects/SDE Lighthouse 10 - Crosbie/extraction-sql/2.comorbidities.sql
@@ -1,3 +1,5 @@
+USE SCHEMA SDE_REPOSITORY.SHARED_UTILITIES;
+
 --┌───────────────────────────────────────────────────┐
 --│ SDE Lighthouse study 10 - Crosbie - comorbidities │
 --└───────────────────────────────────────────────────┘
@@ -21,12 +23,14 @@ SELECT
 	"PeripheralArterialDisease_DiagnosisDate", "ProstateDisorder_DiagnosisDate", "Psoriasis_DiagnosisDate",
 	"RheumatoidArthritis_DiagnosisDate", "Stroke_DiagnosisDate", "ThyroidDisorder_DiagnosisDate", "TIA_DiagnosisDate"
 FROM INTERMEDIATE.GP_RECORD."LongTermConditionRegister_Diagnosis"
-WHERE "GmPseudo" IN (SELECT "GmPseudo" FROM {{cohort-table}})
+WHERE "GmPseudo" IN (SELECT "GmPseudo" FROM SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie")
 QUALIFY row_number() OVER (PARTITION BY "GmPseudo" ORDER BY "Snapshot" DESC) = 1; -- this brings back the values from the most recent snapshot
 
 -- load codesets for conditions not in above LTC table
 
---> CODESET myocardial-infarction:1 angina:1 tuberculosis:1 venous-thromboembolism:1 pneumonia:1 copd:1 efi-mobility-problems:1 efi-vision-problems:1
+-- >>> Codesets required... Inserting the code set code
+-- >>> Codesets extracted into 0.code-sets.sql
+-- >>> Following code sets injected: myocardial-infarction v1/angina v1/tuberculosis v1/venous-thromboembolism v1/pneumonia v1/copd v1/efi-mobility-problems v1/efi-vision-problems v1
 
 --  create table of conditions not included in LTC table
 
@@ -40,11 +44,11 @@ SELECT DISTINCT
 	, cs.concept
 	, e."Term" AS "Description"
 FROM INTERMEDIATE.GP_RECORD."GP_Events_SecondaryUses" e
-LEFT JOIN {{code-set-table}} cs ON cs.code = e."SuppliedCode"
+LEFT JOIN SDE_REPOSITORY.SHARED_UTILITIES."Code_Sets_SDE_Lighthouse_10_Crosbie" cs ON cs.code = e."SuppliedCode"
 LEFT OUTER JOIN PRESENTATION.GP_RECORD."DemographicsProtectedCharacteristics_SecondaryUses" dem ON dem."FK_Patient_ID" = e."FK_Patient_ID" -- join to demographics table to get GmPseudo
 WHERE cs.concept IN ('myocardial-infarction', 'angina', 'tuberculosis', 'venous-thromboembolism', 
 					'pneumonia', 'copd', 'emphysema', 'chronic-bronchitis', 'efi-mobility-problems', 'efi-vision-problems')
-	AND e."FK_Patient_ID" IN (SELECT "FK_Patient_ID" FROM {{cohort-table}});
+	AND e."FK_Patient_ID" IN (SELECT "FK_Patient_ID" FROM SDE_REPOSITORY.SHARED_UTILITIES."Cohort_SDE_Lighthouse_10_Crosbie");
 
 -- for each patient, find each comorbidity and the date of first diagnosis
 
@@ -76,7 +80,15 @@ GROUP BY "GmPseudo";
 
 -- FINAL TABLE JOIING TOGETHER THE STANDARD DIAGNOSES AND THE OTHERS
 
-{{create-output-table::"2_Comorbidities"}}
+
+-- ... processing [[create-output-table::"2_Comorbidities"]] ... 
+-- ... Need to create an output table called "2_Comorbidities" and replace 
+-- ... the GmPseudo column with a study-specific random patient id.
+
+-- First we create a table in an area only visible to the RDEs which contains
+-- the GmPseudos. THESE CANNOT BE RELEASED TO END USERS.
+DROP TABLE IF EXISTS SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities_WITH_PSEUDO_IDS";
+CREATE TABLE SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities_WITH_PSEUDO_IDS" AS
 SELECT 
 	ltc.*,
 	ot."MyocardialInfarction_DiagnosisDate",
@@ -91,4 +103,36 @@ SELECT
 	ot."VisionProblems_DiagnosisDate"
 FROM StandardLTCs ltc
 LEFT JOIN OtherDiagsSummaryWide	 ot ON ot."GmPseudo" = ltc."GmPseudo";
+
+-- Then we check to see if there are any new GmPseudo ids. We do this by making a temp table 
+-- of all "new" GmPseudo ids. I.e. any GmPseudo ids that we've already got a unique id for
+-- for this study are excluded
+DROP TABLE IF EXISTS "AllPseudos_SDE_Lighthouse_10_Crosbie";
+CREATE TEMPORARY TABLE "AllPseudos_SDE_Lighthouse_10_Crosbie" AS
+SELECT DISTINCT "GmPseudo" FROM SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities_WITH_PSEUDO_IDS"
+EXCEPT
+SELECT "GmPseudo" FROM "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie";
+
+-- Find the highest currently assigned id. Ids are given incrementally, so now ones
+-- need to start at +1 of the current highest
+SET highestPatientId = (
+    SELECT IFNULL(MAX("StudyPatientPseudoId"),0) FROM "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie"
+);
+
+-- Make a study specific hash for each new GmPseudo and insert it
+-- into the patient lookup table
+INSERT INTO "Patient_ID_Mapping_SDE_Lighthouse_10_Crosbie"
+SELECT
+    "GmPseudo", -- the GM SDE patient ids for patients in this cohort
+    SHA2(CONCAT('SDE_Lighthouse_10_Crosbie', "GmPseudo")) AS "Hash", -- used to provide a random (study-specific) ordering for the patient ids we provide
+    $highestPatientId + ROW_NUMBER() OVER (ORDER BY "Hash") -- the patient id that we provide to the analysts
+FROM "AllPseudos_SDE_Lighthouse_10_Crosbie";
+
+-- Finally, we select from the output table which includes the GmPseudos, in order
+-- to populate the table for the end users where the GmPseudo fields are redacted via a function
+-- created in the 0.code-sets.sql file
+DROP TABLE IF EXISTS SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities";
+CREATE TABLE SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities" AS
+SELECT SDE_REPOSITORY.SHARED_UTILITIES.gm_pseudo_hash_SDE_Lighthouse_10_Crosbie("GmPseudo") AS "PatientID", * EXCLUDE "GmPseudo"
+FROM SDE_REPOSITORY.SHARED_UTILITIES."2_Comorbidities_WITH_PSEUDO_IDS";
 
